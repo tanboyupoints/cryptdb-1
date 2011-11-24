8f2953ac025a1a4fa30888b2a66888685f363388
cat_red <cat_red@mit.edu>
2011-11-24 17:25:59 -0500

    added parser/Annotation and parser/Analysis files

---
+++ parser/Analysis.hh
@@ -0,0 +1,155 @@
+#pragma once
+
+#include <crypto-old/CryptoManager.hh>
+#include <util/onions.hh>
+
+#include <parser/embedmysql.hh>
+#include <parser/stringify.hh>
+
+using namespace std;
+
+struct FieldMeta;
+/**
+ * Field here is either:
+ * A) empty string, representing any field or
+ * B) the field that the onion is key-ed on. this
+ *    only has semantic meaning for DET and OPE
+ */
+typedef std::pair<SECLEVEL, FieldMeta *> LevelFieldPair;
+typedef std::map<onion, LevelFieldPair>  OnionLevelFieldMap;
+typedef std::pair<onion, LevelFieldPair> OnionLevelFieldPair;
+typedef std::map<onion, SECLEVEL>        OnionLevelMap;
+
+
+/**
+ * Use to keep track of a field's encryption onions.
+ */
+class EncDesc {
+public:
+    EncDesc(OnionLevelMap input) : olm(input) {}
+    EncDesc(const EncDesc & ed) : olm(ed.olm) {}
+    /**
+     * Returns true if something was changed, false otherwise.
+     */
+    bool restrict(onion o, SECLEVEL maxl);
+
+    OnionLevelMap olm;
+};
+
+struct TableMeta;
+//TODO: FieldMeta and TableMeta are partly duplicates with the original
+// FieldMetadata an TableMetadata
+// which contains data we want to add to this structure soon
+// we can remove old ones when we have needed functionality here
+typedef struct FieldMeta {
+    TableMeta * tm; //point to table belonging in
+    std::string fname;
+    Create_field * sql_field;
+
+    map<onion, string> onionnames;
+    EncDesc encdesc;
+
+    bool has_salt; //whether this field has its own salt
+    std::string salt_name;
+
+    FieldMeta();
+
+    inline bool hasOnion(onion o) const {
+        return encdesc.olm.find(o) !=
+            encdesc.olm.end();
+    }
+
+    inline SECLEVEL getOnionLevel(onion o) const {
+        auto it = encdesc.olm.find(o);
+        if (it == encdesc.olm.end()) return SECLEVEL::INVALID;
+        return it->second;
+    }
+
+} FieldMeta;
+
+
+typedef struct TableMeta {
+
+    std::list<std::string> fieldNames;     //in order field names                     
+    unsigned int tableNo;
+    std::string anonTableName;
+
+    std::map<std::string, FieldMeta *> fieldMetaMap;
+
+    bool hasSensitive;
+
+    bool has_salt;
+    std::string salt_name;
+
+    TableMeta();
+    ~TableMeta();
+} TableMeta;
+
+typedef struct SchemaInfo {
+    std::map<std::string, TableMeta *> tableMetaMap;
+    unsigned int totalTables;
+    embedmysql * embed_db;
+
+    SchemaInfo():totalTables(0) {};
+    ~SchemaInfo() {cerr << "called schema destructor"; tableMetaMap.clear();}
+    TableMeta * getTableMeta(const string & table);
+    FieldMeta * getFieldMeta(const string & table, const string & field);
+} SchemaInfo;
+
+/***************************************************/
+
+// metadata for field analysis                                                        
+class FieldAMeta {
+public:
+    EncDesc exposedLevels; //field identifier to max sec level allowed to process a query
+    FieldAMeta(const EncDesc & ed) : exposedLevels(ed) {}
+};
+
+//Metadata about how to encrypt an item
+class ItemMeta {
+public:
+    onion o;
+    SECLEVEL uptolevel;
+    FieldMeta * basefield;
+};
+extern "C" void *create_embedded_thd(int client_flag);
+
+typedef struct ReturnField {
+    bool is_salt;
+    ItemMeta *im;
+    int pos_salt; //position of salt of this field in the query results,
+                  // or -1 if such salt was not requested
+} ReturnField;
+
+typedef struct ReturnMeta {
+    map<int, ReturnField> rfmeta;
+} ReturnMeta;
+
+
+class Analysis {
+public:
+    Analysis(MYSQL *m, SchemaInfo * schema, CryptoManager *cm)
+        : pos(0), schema(schema), cm(cm), m(m) {
+        assert(m != NULL);
+    }
+    Analysis(): pos(0), schema(NULL), cm(NULL), m(NULL) {
+    }
+    inline MYSQL* conn() {
+        mysql_thread_init();
+        return m;
+    }
+
+    unsigned int pos; //a counter indicating how many projection fields have been analyzed so far                                                                    
+    std::map<std::string, FieldAMeta *> fieldToAMeta;
+    std::map<Item*, ItemMeta *>         itemToMeta;
+    std::map<Item_field*, FieldMeta*>   itemToFieldMeta;
+    std::set<Item*>                     itemHasRewrite;
+    SchemaInfo *                        schema;
+    CryptoManager *                     cm;
+
+    ReturnMeta rmeta;
+
+private:
+    MYSQL * m;
+
+};
---
+++ parser/Annotation.cc
@@ -0,0 +1,237 @@
+//removes character c from the end of s, if it's the last character
+// if c isn't the last character, returns without doing anything
+static void
+end_prune(string &str, char c) {
+    if (*str.rbegin() == c) {
+        str.erase(str.rbegin());
+    }
+}
+
+//splits string s into a list of words deliminated by c
+static list<string>
+split(string &str, char c) {
+    string s = str;
+    list<string> result;
+    size_t delim;
+    while((delim = s.find(c)) != string::npos) {
+        string word = s.substr(0,delim);
+        result.push_back(word);
+        s = s.substr(delim,string::npos);
+    }
+    return result;
+}
+
+Annotation::Annotation(const string &q) {
+    query = q;
+    parse();
+}
+
+PrincType
+Annotation::getLeft() {
+    if (type == SPEAKSFOR) {
+        return left;
+    }
+    LOG(error) << "Annotation asked for left when not SPEAKSFOR";
+    PrincType empty;
+    empty.princtype = "";
+    empty.column = "";
+    return empty;
+}
+
+PrincType
+Annotation::getRight() {
+    if (type == SPEAKSFOR || type == ENCFOR) {
+        return right;
+    }
+    LOG(error) << "Annotation asked for right when not SPEAKSFOR or ENCFOR";
+    PrincType empty;
+    empty.princtype = "";
+    empty.column = "";
+    return empty;
+}
+
+string
+Annotation::getPrimitive() {
+    if (type != SPEAKSFOR) {
+        return primitive;
+    }
+    LOG(error) << "Annotation asked for primitive when SPEAKSFOR";
+    return "";
+}
+
+string
+Annotation::getLeftTableName() {
+    if (type != SPEAKSFOR) {
+        LOG(error) << "Annotation asked for left when not SPEAKSFOR";
+        return "";
+    }
+    list<string> left_words = split(left.column, '.');
+    assert_s(left_words.size() == 2, "table or column name has a . in it");
+    return *left_words.begin();
+}
+
+string
+Annotation::getRightTableName() {
+    if (type != SPEAKSFOR && type != ENCFOR) {
+        LOG(error) << "Annotation asked for right when not SPEAKSFOR or ENCFOR";
+        return "";
+    }
+    list<string> right_words = split(right.column, '.');
+    assert_s(right_words.size() == 2, "table or column name has a . in it");
+    return *right_words.begin();
+}
+
+string
+Annotation::getPrimitiveTableName() {
+    if (type != ENCFOR) {
+        LOG(error) << "Annotation asked for primitive when not ENCFOR";
+        return "";
+    }
+    list<string> prim_words = split(primitive, '.');
+    assert_s(prim_words.size() == 2, "table or column name has a . in it");
+    return *prim_words.begin();
+}
+
+//XXX
+//enclevel encoding stuff
+
+Predicate *
+Annotation::getPredicate() {
+    if (type != SPEAKSFOR) {
+        LOG(error) << "Annotation asked for predicate when not SPEAKSFOR";
+        return NULL;
+    }
+    return pred;
+}
+
+string
+getLeftStr() {
+    if (type == SPEAKSFOR) {
+        return left.princtype + "=" + left.column;
+    }
+    LOG(error) << "Annotation asked for left when not SPEAKSFOR";
+    return "";
+}
+
+string
+getLeftStr() {
+    if (type == SPEAKSFOR || ENCFOR) {
+        return right.princtype + "=" + right.column;
+    }
+    LOG(error) << "Annotation asked for left when not SPEAKSFOR";
+    return "";
+}    
+
+void
+Annotation::parse() {
+    //remove trailing semicolon
+    prune(&query, ';');
+    list<string> query_list = split(&query, ' ');
+    
+    //type
+    if (toLowerCase(query).find("princtype",0) != string::npos) {
+        if (toLowerCase(query).find("external",0) != string::npos) {
+            type = PRINCTYPE_EXTERNAL;
+        } else {
+            type = PRINCTYPE;
+        }
+    } else if (toLowerCase(query).find("encfor",0) != string::npos) {
+        type = ENCFOR;
+    } else if (toLowerCase(query).find("speaksfor",0) != string::npos) {
+        type = SPEAKSFOR;
+    } else {
+        assert_s(false, "annotation is not PRINCTYPE, PRINCTYPE EXTERNAL, SPEAKSFOR, or ENCFOR");
+    }
+
+    auto word = query_list.begin();
+    if (toLowerCase(*word) == "cryptdb") {
+        word++;
+    }
+
+    //first word/pair
+    switch (type) {
+    case PRINCTYPE:
+    case PRINCTYPE EXTERNAL:
+        assert_s(toLowerCase(*word) == "princtype", "PRINCTYPE annotation does not begin with PRINCTYPE");
+        break;
+    case ENCFOR:
+        primitive = *word;
+        break;
+    case SPEAKSFOR:
+        left.princtype = *word;
+        word++;
+        left.column = *word;
+        break;
+    }
+
+    //middle word
+    word++;
+    switch(type) {
+    case PRINCTYPE:
+    case PRINCTYPE EXTERNAL:
+        primitive = *word;
+        break;
+    case ENCFOR:
+        assert_s(toLowerCase(*word) == "encfor", "middle of ENCFOR annotation is not ENCFOR");
+        break;
+    case SPEAKSFOR:
+        assert_s(toLowerCase(*word) == "speaksfor", "middle of SPEAKSFOR annotation is not SPEAKSFOR");
+        break;
+    }
+
+    //last word/pair
+    word++;
+    switch(type) {
+    case PRINCTYPE:
+        assert_s(word == query_list.end(), "PRINCTYPE annotation has too many words");
+        return;
+    case PRINCTYPE_EXTERNAL:
+        assert_s(toLowerCase(*word) == "external", "PRINCTYPE EXERNAL annotation does not have EXTERNAL after primitive");
+    case ENCFOR:
+    case SPEAKSFOR:
+        right.princtype = *word;
+        word++;
+        right.column = *word;
+    }
+
+    //encryption level or predicate, if exists
+    word++;
+    switch(type) {
+    case PRINCTYPE:
+    case PRINCTYPE_EXTERNAL:
+        //this should be the end
+        assert_s(word == query_list.end(), "annotation has too many words");
+        break;
+    case ENCFOR:
+        if (word == query_list.end()) {
+            break;
+        }
+        if (equalsIgnoreCase(levelnames[(int) SECLEVEL::DET], *word)) {
+            DETenclevel = SECLEVEL::DET;
+            break;
+        }
+        if (equalsIgnoreCase(levelnames[(int) SECLEVEL::DETJOIN], *word)) {
+            DETenclevel = SECLEVEL::DETJOIN;
+            break;
+        }
+        if (equalsIgnoreCase(levelnames[(int) SECLEVEL::OPE], *word)) {
+            DETenclevel = SECLEVEL::DET;
+            OPEenclevel = SECLEVEL::OPE;
+            break;
+        }
+        if (equalsIgnorCase(levelnames[(int) SECLEVEL::SEMANTIC_AGG], *word)) {
+            AGGenclevel = true;
+            break;
+        }
+    case SPEAKSFOR:
+        assert_s(toLowerCase(word) == "if", "SPEAKSFOR annotation predicate does not start with if");
+        list<string> pred_split = split(*word, '(');
+        assert_s(pred_split.size() == 2, "predicate has too many (");
+        pred->name = *pred_split.begin();
+        prune(*pred_split.end(),')');
+        list<string> pred_fields = split(*pred_split.end(),',');
+        for(auto field = pred_fields.begin(); field != pred_fields.end(); field++) {
+            pred->fields.push_back(word);
+        }
+    }
+}
