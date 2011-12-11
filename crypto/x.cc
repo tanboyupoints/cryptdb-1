63246764904fdbaca33901f37810b1dd2ae752a1
Nickolai Zeldovich <nickolai@csail.mit.edu>
2011-12-11 10:23:47 -0800

    ah, for non-multiple-of-8-bits, the problem was in the test case not clearing remaining bits, rather than in ffx implementation. seems to work just fine now.

--- crypto/x.cc
+++ crypto/x.cc
@@ -221,10 +221,11 @@ test_ffx()
 
     for (int i = 0; i < 100; i++) {
         uint nbits = 8 + (rnd.rand<uint>() % 121);
-        nbits = (nbits / 8) * 8;
-
         auto pt = rnd.rand_vec<uint8_t>((nbits + 7) / 8);
         auto t = rnd.rand_vec<uint8_t>(rnd.rand<uint>() % 1024);
+
+        uint lowbits = nbits % 8;
+        pt[(nbits-1) / 8] &= ~0 << (8 - lowbits);
 
         std::vector<uint8_t> ct, pt2;
         ct.resize(pt.size());
