810f299557efc5b896d7fc51575c1e14b02c5f95
Stephen Tu <stephentu@csail.mit.edu>
2012-03-10 01:33:08 -0500

    make units consistent for mont

--- crypto/mont.cc
+++ crypto/mont.cc
@@ -2,6 +2,9 @@
 #include <assert.h>
 #include <crypto/mont.hh>
 
+#include <gmp.h>
+#include <util/static_assert.hh>
+
 using namespace std;
 using namespace NTL;
 
@@ -17,13 +20,17 @@ montgomery::from_mont(const ZZ &a)
     return MulMod(a, _rinv, _m);
 }
 
+#define SIZE(p) (((long *) (p))[1])
+#define DATA(p) ((mp_limb_t *) (((long *) (p)) + 2))
+
 ZZ
 montgomery::mmul(const ZZ &a, const ZZ &b)
 {
+    _static_assert( sizeof(mp_limb_t) == sizeof(long) );
     ZZ ab = a * b;
 
-    for (uint i = 0; i < _mbits; i += sizeof(long)) {
-        uint thisbits = std::min((uint) sizeof(long), _mbits - i);
+    for (uint i = 0; i < _mbits; i += sizeof(long) * 8) {
+        uint thisbits = std::min((uint) sizeof(long) * 8, _mbits - i);
         long l = trunc_long(ab, thisbits);  // bits to cancel out
         long c = _minusm_inv_modr * l & (~(long)0 >> (sizeof(long)-thisbits));
         ab += _m * c;
--- crypto/mont.hh
+++ crypto/mont.hh
@@ -14,7 +14,7 @@ class montgomery {
     montgomery(const NTL::ZZ &m)
         : _m(m), _mbits(NumBits(m)), _r(NTL::to_ZZ(1) << _mbits),
           _rinv(InvMod(_r % _m, _m)),
-          _minusm_inv_modr(trunc_long(InvMod(_r-_m, _r), sizeof(long))) {}
+          _minusm_inv_modr(trunc_long(InvMod(_r-_m, _r), sizeof(long) * 8)) {}
 
     NTL::ZZ to_mont(const NTL::ZZ &a);
     NTL::ZZ from_mont(const NTL::ZZ &a);
