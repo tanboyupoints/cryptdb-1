a4e416d8f7bce76a37ee26b5d52d59423b79d83c
Nickolai Zeldovich <nickolai@csail.mit.edu>
2012-03-09 23:37:23 -0500

    slightly less slow, but getting faster may require direct gmp manipulation

--- crypto/mont.cc
+++ crypto/mont.cc
@@ -20,17 +20,18 @@ montgomery::from_mont(const ZZ &a)
 ZZ
 montgomery::mmul(const ZZ &a, const ZZ &b)
 {
-    ZZ abr = a * b;
+    ZZ ab = a * b;
 
     for (uint i = 0; i < _mbits; i += sizeof(long)) {
         uint thisbits = std::min((uint) sizeof(long), _mbits - i);
-        long l = trunc_long(abr >> i, thisbits);  // bits to cancel out
+        long l = trunc_long(ab, thisbits);  // bits to cancel out
         long c = _minusm_inv_modr * l & (~(long)0 >> (sizeof(long)-thisbits));
-        abr += (_m * c) << i;
+        ab += _m * c;
+
+        // assert(trunc_long(ab, thisbits) == 0);
+        ab = ab >> thisbits;
     }
 
-    // assert(abr % _r == 0);
-    ZZ ab = abr >> _mbits;
     while (ab >= _m)
         ab = ab - _m;
     return ab;
