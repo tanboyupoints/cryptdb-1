c737a46b019c56bd23c9333e2cd13d1b5e501979
Nickolai Zeldovich <nickolai@csail.mit.edu>
2012-03-09 23:33:42 -0500

    closer to alg 14.36 but now way slow

--- crypto/mont.cc
+++ crypto/mont.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <assert.h>
 #include <crypto/mont.hh>
 
@@ -20,14 +21,17 @@ ZZ
 montgomery::mmul(const ZZ &a, const ZZ &b)
 {
     ZZ abr = a * b;
-    ZZ l = trunc_ZZ(abr, _mbits);   // low bits that need to be shot down
-    ZZ c = trunc_ZZ(l * _minusm_inv_modr, _mbits);
 
-    // assert((abr + c * _m) % _r == 0);
+    for (uint i = 0; i < _mbits; i += sizeof(long)) {
+        uint thisbits = std::min((uint) sizeof(long), _mbits - i);
+        long l = trunc_long(abr >> i, thisbits);  // bits to cancel out
+        long c = _minusm_inv_modr * l & (~(long)0 >> (sizeof(long)-thisbits));
+        abr += (_m * c) << i;
+    }
 
-    ZZ ab = (abr + c * _m) >> _mbits;
-    if (ab >= _m)
-        return ab - _m;
-    else
-        return ab;
+    // assert(abr % _r == 0);
+    ZZ ab = abr >> _mbits;
+    while (ab >= _m)
+        ab = ab - _m;
+    return ab;
 }
--- crypto/mont.hh
+++ crypto/mont.hh
@@ -8,13 +8,13 @@ class montgomery {
     uint _mbits;
     NTL::ZZ _r;
     NTL::ZZ _rinv;
-    NTL::ZZ _minusm_inv_modr;
+    long _minusm_inv_modr;
 
  public:
     montgomery(const NTL::ZZ &m)
         : _m(m), _mbits(NumBits(m)), _r(NTL::to_ZZ(1) << _mbits),
           _rinv(InvMod(_r % _m, _m)),
-          _minusm_inv_modr(InvMod(_r-_m, _r)) {}
+          _minusm_inv_modr(trunc_long(InvMod(_r-_m, _r), sizeof(long))) {}
 
     NTL::ZZ to_mont(const NTL::ZZ &a);
     NTL::ZZ from_mont(const NTL::ZZ &a);
