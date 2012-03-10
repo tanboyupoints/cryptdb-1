7c92d3d19c37199ca5541d9c08bd788bedeb631c
Nickolai Zeldovich <nickolai@csail.mit.edu>
2012-03-09 23:14:36 -0500

    correct but still slow

--- crypto/mont.cc
+++ crypto/mont.cc
@@ -21,9 +21,9 @@ montgomery::mmul(const ZZ &a, const ZZ &b)
 {
     ZZ abr = a * b;
     ZZ l = trunc_ZZ(abr, _mbits);   // low bits that need to be shot down
-    ZZ c = l * _minusm_inv_modr;
+    ZZ c = trunc_ZZ(l * _minusm_inv_modr, _mbits);
 
-    // assert((abr + c) % _r == 0);
+    // assert((abr + c * _m) % _r == 0);
 
     ZZ ab = (abr + c * _m) >> _mbits;
     if (ab >= _m)
