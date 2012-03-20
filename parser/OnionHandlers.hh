61c60a296170836f9edc4734ba35e222443ce928
Nickolai Zeldovich <nickolai@csail.mit.edu>
2012-03-20 01:58:18 -0400

    some ideas on the onion layer api

--- parser/OnionHandlers.hh
+++ parser/OnionHandlers.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <util/util.hh>
+#include <crypto/prng.hh>
 
 #include <sql_select.h>
 #include <sql_delete.h>
@@ -33,9 +34,19 @@ public:
     
 };
 
-typedef struct cdb_key {
-    
-} cdb_key;
+// One possible plan: an onion layer key is logically a PRNG that
+// generates a suitable key.
+//
+// In practice, the PRNG would be something like blockrng<AES> based
+// on the master AES key, or based on a multiprinc key.
+//
+// Each onion layer reads the necessary number of bytes from the
+// PRNG to generate its key (AES, blowfish, Paillier, SWP, ...)
+//
+// The naive version of this is likely inefficient (it will require
+// performing key setup each time), but it should be possible to
+// retrofit a suitable caching scheme once everything else works.
+typedef PRNG cdb_key;
 
 //TODO: currently, we have one such object per onion layer per field
 // optimize storage by sharing these handlers among objects with same type
@@ -45,55 +56,43 @@ public:
     
     virtual Item * encrypt(cdb_key * key, Item * ptext, uint64_t IV = 0) = 0;
     virtual Item * decrypt(cdb_key * key, Item * i, uint64_t IV = 0) = 0;
-
-protected:
-
+    virtual Item * decryptUDF(cdb_key * key, Item *columnref, Item *ivcolumnref = 0) {
+        thrower() << "decryptUDF not supported";
+    }
 };
 
-class OnionLayerRND : OnionLayer {
+class OnionLayerRND : public OnionLayer {
 public:
     Create_field * layerCreateField(Create_field *);
     
-    Item * encrypt(cdb_key * key, Item * ptext, uint64_t IV = 0);
-    Item * decrypt(cdb_key * key, Item * i, uint64_t IV = 0);
-
-    std::string decryptUDF(std::string field);
+    Item * encrypt(cdb_key * key, Item * ptext, uint64_t IV);
+    Item * decrypt(cdb_key * key, Item * i, uint64_t IV);
+    Item * decryptUDF(cdb_key *key, Item *columnref, Item *ivcolumnref);
 };
 
-class OnionLayerDET : OnionLayer {
+class OnionLayerDET : public OnionLayer {
 public:
     Create_field * layerCreateField(Create_field *);
     
-    Item * encrypt(cdb_key * key, Item * ptext, uint64_t IV = 0);
-    Item * decrypt(cdb_key * key, Item * i, uint64_t IV = 0);
-
-    std::string decryptUDF(std::string field);
+    Item * encrypt(cdb_key * key, Item * ptext, uint64_t IV);
+    Item * decrypt(cdb_key * key, Item * i, uint64_t IV);
+    Item * decryptUDF(cdb_key *key, Item *columnref, Item *ivcolumnref);
 };
 
-class OnionLayerHOM : OnionLayer {
+class OnionLayerHOM : public OnionLayer {
 public:
     Create_field * layerCreateField(Create_field *);
     
-    Item * encrypt(cdb_key * key, Item * ptext, uint64_t IV = 0);
-    Item * decrypt(cdb_key * key, Item * i, uint64_t IV = 0);
-
-    std::string sumUDF(cdb_key * key, std::string field);
-    
+    Item * encrypt(cdb_key * key, Item * ptext, uint64_t IV);
+    Item * decrypt(cdb_key * key, Item * i, uint64_t IV);
+    Item * sumUDF(cdb_key * key, Item *columnref);
 };
 
-class OnionLayerSearch : OnionLayer {
+class OnionLayerSearch : public OnionLayer {
 public:
     Create_field * layerCreateField(Create_field *);
     
-    Item * encrypt(cdb_key * key, Item * ptext, uint64_t IV = 0);
-    Item * decrypt(cdb_key * key, Item * i, uint64_t IV = 0);
-
-    std::string seachUDF(cdb_key * key, std::string field, std::string token);
+    Item * encrypt(cdb_key * key, Item * ptext, uint64_t IV);
+    Item * decrypt(cdb_key * key, Item * i, uint64_t IV);
+    Item * seachUDF(cdb_key * key, Item *columnref, std::string keyword);
 };
-
-    
-
-  
-
-
-
