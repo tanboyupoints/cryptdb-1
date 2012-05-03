b6d3cc8ec7bb8f818a4c6d68319f8d40c75c52b9
cat_red <cat_red@mit.edu>
2012-05-03 00:25:55 -0400

    Revert "proxy re-added to makefile, but rewriter currently segfaults on queries it is not supposed to deal with, like : select @@version_comment limit 1"
    This reverts commit 0a82aeb64ca1ab5f630be8415b3b881847e5b75e.

--- mysqlproxy/ConnectWrapper.cc
+++ mysqlproxy/ConnectWrapper.cc
@@ -7,8 +7,7 @@
 #include <util/cryptdb_log.hh>
 #include <util/scoped_lock.hh>
 
-#include <parser/cdb_rewrite.hh>
-#include <edb/sql_utils.hh>
+#include <edb/EDBProxy.hh>
 
 
 using namespace std;
@@ -18,13 +17,11 @@ class WrapperState {
     string last_query;
     bool considered;
     ofstream * PLAIN_LOG;
-    Analysis a;
 };
 
 static Timer t;
 
-//static EDBProxy * cl = NULL;
-static Rewriter * r = NULL;
+static EDBProxy * cl = NULL;
 static pthread_mutex_t big_lock;
 
 static bool DO_CRYPT = true;
@@ -40,24 +37,6 @@ static string PLAIN_BASELOG = "";
 static int counter = 0;
 
 static map<string, WrapperState*> clients;
-
-static Item *
-make_item(string value, enum_field_types type) {
-    Item * i;
-    switch(type) {
-    case MYSQL_TYPE_SHORT:
-    case MYSQL_TYPE_LONG:
-    case MYSQL_TYPE_LONGLONG:
-    case MYSQL_TYPE_INT24:
-        i = new Item_int((long long) valFromStr(value));
-    case MYSQL_TYPE_VARCHAR:
-    case MYSQL_TYPE_VAR_STRING:
-        i = new Item_string(make_thd_string(value), value.length(), &my_charset_bin);
-    default:
-        assert_s(false, "ConnectWrapper does not know that datatype");
-    }
-    return i;
-}
 
 static string
 xlua_tolstring(lua_State *l, int index)
@@ -88,9 +67,6 @@ connect(lua_State *L)
     string user = xlua_tolstring(L, 4);
     string psswd = xlua_tolstring(L, 5);
     string dbname = xlua_tolstring(L, 6);
-    string embed_dir = xlua_tolstring(L, 7);
-    
-    ConnectionInfo ci = ConnectionInfo(server, user, psswd, port);
 
     WrapperState *ws = new WrapperState();
 
@@ -100,7 +76,7 @@ connect(lua_State *L)
 
     clients[client] = ws;
 
-    if (!r) {
+    if (!cl) {
       cerr << "starting proxy\n";
         cryptdb_logger::setConf(string(getenv("CRYPTDB_LOG")?:""));
 
@@ -112,18 +88,15 @@ connect(lua_State *L)
 
         string mode = getenv("CRYPTDB_MODE")?:"";
         if (mode == "single") {
-            //cl = new EDBProxy(server, user, psswd, dbname, port, false);
-            r = new Rewriter(ci, embed_dir, false, false);
+            cl = new EDBProxy(server, user, psswd, dbname, port, false);
         } else if (mode == "multi") {
-            //cl = new EDBProxy(server, user, psswd, dbname, port, true);
-            r = new Rewriter(ci, embed_dir, true, false);
+            cl = new EDBProxy(server, user, psswd, dbname, port, true);
         } else {
-            //cl = new EDBProxy(server, user, psswd, dbname, port);
-            r = new Rewriter(ci, embed_dir);
+            cl = new EDBProxy(server, user, psswd, dbname, port);
         }
 
         uint64_t mkey = 113341234;  // XXX do not change as it's used for tpcc exps
-        r->setMasterKey(BytesFromInt(mkey, AES_KEY_BYTES));
+        cl->setMasterKey(BytesFromInt(mkey, AES_KEY_BYTES));
 
         //may need to do training
         char * ev = getenv("TRAIN_QUERY");
@@ -131,8 +104,8 @@ connect(lua_State *L)
             string trainQuery = ev;
             LOG(wrapper) << "proxy trains using " << trainQuery;
             if (trainQuery != "") {
-                Analysis a;
-                r->rewrite(trainQuery, a);
+                bool consider;
+                cl->rewriteEncryptQuery(trainQuery, consider);
             } else {
                 cerr << "empty training!\n";
             }
@@ -163,9 +136,8 @@ connect(lua_State *L)
 
         ev = getenv("LOAD_ENC_TABLES");
         if (ev) {
-            cerr << "No current functionality for loading tables\n";
-            //cerr << "loading enc tables\n";
-            //cl->loadEncTables(string(ev));
+            cerr << "loading enc tables\n";
+            cl->loadEncTables(string(ev));
         }
 
         ev = getenv("LOG_PLAIN_QUERIES");
@@ -235,7 +207,7 @@ rewrite(lua_State *L)
 
     string query = xlua_tolstring(L, 2);
 
-    //clients[client]->considered = true;
+    clients[client]->considered = true;
 
     list<string> new_queries;
 
@@ -245,7 +217,7 @@ rewrite(lua_State *L)
             new_queries.push_back(query);
         } else {
             try {
-                new_queries = r->rewrite(query, clients[client]->a);
+                new_queries = cl->rewriteEncryptQuery(query, clients[client]->considered);
                 //cerr << "query: " << *new_queries.begin() << " considered ? " << clients[client]->considered << "\n";
             } catch (CryptDBError &e) {
                 LOG(wrapper) << "cannot rewrite " << query << ": " << e.msg;
@@ -260,8 +232,8 @@ rewrite(lua_State *L)
         *(clients[client]->PLAIN_LOG) << query << "\n";
     }
 
-    //lua_pushboolean(L, clients[client]->considered);
-    lua_pushboolean(L, true);
+    lua_pushboolean(L, clients[client]->considered);
+    //lua_pushboolean(L, true);
 
     lua_createtable(L, (int) new_queries.size(), 0);
     int top = lua_gettop(L);
@@ -286,7 +258,7 @@ decrypt(lua_State *L)
     if (clients.find(client) == clients.end())
         return 0;
 
-    ResType res;
+    ResType r;
 
     /* iterate over the fields argument */
     lua_pushnil(L);
@@ -298,9 +270,9 @@ decrypt(lua_State *L)
         while (lua_next(L, -2)) {
             string k = xlua_tolstring(L, -2);
             if (k == "name")
-                res.names.push_back(xlua_tolstring(L, -1));
+                r.names.push_back(xlua_tolstring(L, -1));
             else if (k == "type")
-                res.types.push_back((enum_field_types) luaL_checkint(L, -1));
+                r.types.push_back((enum_field_types) luaL_checkint(L, -1));
             else
                 LOG(warn) << "unknown key " << k;
             lua_pop(L, 1);
@@ -316,27 +288,28 @@ decrypt(lua_State *L)
             LOG(warn) << "mismatch";
 
         /* initialize all items to NULL, since Lua skips nil array entries */
-        vector<Item *> row(res.names.size());
+        vector<SqlItem> row(r.names.size());
 
         lua_pushnil(L);
         while (lua_next(L, -2)) {
             int key = luaL_checkint(L, -2);
             string data = xlua_tolstring(L, -1);
-            Item * value = make_item(data, res.types[key]);
-            row[key - 1] = value;
+            row[key - 1].null = false;
+            row[key - 1].type = MYSQL_TYPE_BLOB;    /* XXX */
+            row[key - 1].data = data;
             lua_pop(L, 1);
         }
 
-        res.rows.push_back(row);
+        r.rows.push_back(row);
         lua_pop(L, 1);
     }
 
     ResType rd;
     if (!DO_CRYPT || !clients[client]->considered) {
-        rd = res;
+        rd = r;
     } else {
         try {
-            rd = r->decryptResults(res, clients[client]->a);
+            rd = cl->decryptResults(clients[client]->last_query, r);
         }
         catch(CryptDBError e) {
             lua_pushnil(L);
@@ -371,10 +344,10 @@ decrypt(lua_State *L)
         int t_row = lua_gettop(L);
 
         for (uint j = 0; j < rd.rows[i].size(); j++) {
-            if (rd.rows[i][j] == NULL) {
+            if (rd.rows[i][j].null) {
                 lua_pushnil(L);
             } else {
-                xlua_pushlstring(L, ItemToString(rd.rows[i][j]));
+                xlua_pushlstring(L, rd.rows[i][j].data);
             }
             lua_rawseti(L, t_row, j+1);
         }
--- mysqlproxy/wrapper.lua
+++ mysqlproxy/wrapper.lua
@@ -15,8 +15,7 @@ function read_auth()
                     proxy.connection.server.dst.port,
                     os.getenv("CRYPTDB_USER") or "root",
                     os.getenv("CRYPTDB_PASS") or "letmein",
-                    os.getenv("CRYPTDB_DB") or "cryptdbtest",
-                    os.getenv("CRYPTDB_SHADOW") or "~/shadow-db")
+                    os.getenv("CRYPTDB_DB") or "cryptdbtest")
     -- EDBClient uses its own connection to the SQL server to set up UDFs
     -- and to manipulate multi-principal state.  (And, in the future, to
     -- store its schema state for single- and multi-principal operation.)
