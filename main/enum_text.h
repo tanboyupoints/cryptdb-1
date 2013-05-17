8d523b6fdc9356e210806325520681253a31c038
burrows <burrows.labs@gmail.com>
2013-05-17 08:14:47 -0400

    Added code to handle enum to text and text to enum conversions for database stuff.

---
+++ main/enum_text.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+template <typename type>
+class EnumText {
+public:
+    EnumText(std::vector<type> enums, std::vector<std::string> texts) :
+        theEnums(enums), theTexts(texts) {}
+
+    ~EnumText() {;}
+
+    // Instance.
+    std::string getText(type e);
+    type getEnum(std::string t);
+
+    // Static.
+    static void addSet(std::vector<type> enums,
+                       std::vector<std::string> text);
+    static std::vector<std::string> allText();
+    static std::vector<type> allEnum();
+    static std::string toText(type e);
+    static type toEnum(std::string t);
+
+protected:
+    // Instance.
+    std::vector<std::string> theTexts;
+    std::vector<type> theEnums;
+    static EnumText *instance;
+};
+
