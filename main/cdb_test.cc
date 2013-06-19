2b56bc758d3b97797e367d062c70ecf20f04e154
Carlos Carvalho <carloslack@gmail.com>
2013-06-19 12:28:06 -0300

    Some cleanup , database name persistence and a sugestion of rennung SQL command in batch mode.

--- main/cdb_test.cc
+++ main/cdb_test.cc
@@ -13,25 +13,47 @@
 #include <functional>
 #include <cctype>
 #include <locale>
-
-#include <main/Connect.hh>
-
 #include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
 
-#include <main/cdb_rewrite.hh>
+#include "main/Connect.hh"
 
 #include <readline/readline.h>
 #include <readline/history.h>
 
-#include <parser/embedmysql.hh>
-#include <parser/stringify.hh>
+#include "main/cdb_rewrite.hh"
+#include "parser/embedmysql.hh"
+#include "parser/stringify.hh"
+#include "crypto/ecjoin.hh"
+#include "util/errstream.hh"
+#include "util/cryptdb_log.hh"
 
-#include <crypto/ecjoin.hh>
-
-#include <util/errstream.hh>
-#include <util/cryptdb_log.hh>
+#define QDIR "./qdir"
 
 using namespace std;
+
+class testBatchCommands {
+    public:
+       map<int, char*> files;
+       DIR *dir;
+       struct dirent *dentry;
+
+       vector<string>populate(string filename);
+};
+
+vector<string>
+testBatchCommands::populate(string filename)
+{
+    vector<string>vec;
+    string tmp = QDIR;
+    tmp += "/" + filename;
+
+    //TODO: open file and fill up vec
+    
+    return vec;
+}
+
 
 // if true, uses MultiPrinc mode
 static bool Multi = false;
@@ -51,43 +73,24 @@ static void __write_history() {
     write_history(user_histfile().c_str());
 }
 
-static inline std::string &ltrim(std::string &s) {
-  s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+static inline string &ltrim(string &s) {
+  s.erase(s.begin(), find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
   return s;
 }
 
-static inline std::string &rtrim(std::string &s) {
-  s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+static inline string &rtrim(string &s) {
+  s.erase(find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(), s.end());
   return s;
 }
 
-static inline std::string &trim(std::string &s) {
+static inline string &trim(string &s) {
   return ltrim(rtrim(s));
 }
 
-static const std::string BOLD_BEGIN = "\033[1m";
-static const std::string RED_BEGIN = "\033[1;31m";
-static const std::string GREEN_BEGIN = "\033[1;92m";
-static const std::string COLOR_END = "\033[0m";
-
-static bool
-isprintable(uint c) {
-    return (c >= 9) && (c <= 126);
-}
-static string prettyquery(string q) {
-    stringstream res;
-
-    for (auto c : q) {
-	if (isprintable((uint)c)) {
-	    res << c;
-	} else {
-	    res << "\\";
-            res << (uint)c;
-	}
-    }
-
-    return res.str();
-}
+static const string BOLD_BEGIN = "\033[1m";
+static const string RED_BEGIN = "\033[1;31m";
+static const string GREEN_BEGIN = "\033[1;92m";
+static const string COLOR_END = "\033[0m";
 
 /** returns true if should stop, to keep looping */
 static bool handle_line(Connect& conn, Rewriter& r, const string& q)
@@ -127,7 +130,11 @@ static bool handle_line(Connect& conn, Rewriter& r, const string& q)
   QueryRewrite qr;
 
   try {
-    string curdb("cryptdbtest");
+    // static initialized database name
+    string curdb(conn.getCurDBName());
+    if(curdb.size() == 0)
+        throw runtime_error("database name not set");
+
     qr = r.rewrite(q, &curdb);
     //only last query should return anything
     if (qr.queries.size() == 0) {
@@ -136,7 +143,7 @@ static bool handle_line(Connect& conn, Rewriter& r, const string& q)
     for (auto new_q = qr.queries.begin(); new_q != qr.queries.end(); new_q++) {
       cerr << endl
            << RED_BEGIN << "ENCRYPTED QUERY:" << COLOR_END << endl
-           << prettyquery(*new_q) << endl;
+           << *new_q << endl;
       assert(conn.execute(*new_q, dbres));
     }
     if (!dbres) {
@@ -156,7 +163,7 @@ static bool handle_line(Connect& conn, Rewriter& r, const string& q)
     cerr << endl << RED_BEGIN << "DECRYPTED RESULTS:" << COLOR_END << endl;
     printRes(dec_res);
 
-  } catch (std::runtime_error &e) {
+  } catch (runtime_error &e) {
     cout << "Unexpected Error: " << e.what() << " in query " << q << endl;
   } catch (CryptDBError &e) {
     cout << "Internal Error: " << e.msg << " in query " << q << endl;
@@ -165,9 +172,27 @@ static bool handle_line(Connect& conn, Rewriter& r, const string& q)
   return true;
 }
 
+
+static string parse_args(const char *msg)
+{
+    string ret;
+    static char cmd[64];
+    memset(cmd, 0, 64);
+
+    cerr << GREEN_BEGIN << msg << COLOR_END;
+    fgets(cmd, 64, stdin);
+    
+    cmd[strlen(cmd)-1] = '\0';
+    ret = cmd;
+
+    return ret;
+}
+
 int
 main(int ac, char **av)
 {
+
+
     if (ac != 2) {
         cerr << "Usage: " << av[0] << " embed-db " << endl;
         exit(1);
@@ -189,12 +214,69 @@ main(int ac, char **av)
 
     string prompt = BOLD_BEGIN + "CryptDB=#" + COLOR_END + " ";
 
+    testBatchCommands batch;
+
+    batch.dir = opendir(QDIR);
+    if(!batch.dir)
+    {
+        cout << "Warning: Could not open dir: " <<  QDIR <<endl;
+    } else
+    {
+        while((batch.dentry = readdir(batch.dir)) != NULL)
+        {
+            static int i = 0;
+            // Fill up files names
+            string tmp = batch.dentry->d_name;
+            if(tmp != "." && tmp != "..")
+                //batch.files.push_back(batch.dentry->d_name);
+                batch.files.insert ( std::pair<int, char*>(i++,batch.dentry->d_name) );
+        }
+    }
+
     for (;;) {
         char *input = readline(prompt.c_str());
         if (!input) break;
         string q(input);
         if (q.empty()) continue;
-        if (!handle_line(conn, r, q))
-          break;
+
+        /*
+         * Alternative options
+         */
+        if(q == "!help")
+        {
+            cout << "!help: Show this help" << endl;
+            cout << "!file: Load mysql commands from file" << endl;
+            continue;
+        } else if(q == "!list")
+        {
+            if(batch.files.size() == 0)
+                continue;
+
+            for(map<int, char*>::iterator it = batch.files.begin(); 
+                    it != batch.files.end(); ++it)
+                cout << "[" << it->first << "] " << it->second << endl; 
+
+        } else if(q == "!file")
+        {
+            string arg = parse_args("CryptDB [Enter id number]=# ");
+            if(arg.size() > 0)
+            {
+                map<int, char*>::iterator it = batch.files.find(atoi(arg.c_str()));
+                if(it != batch.files.end())
+                {
+                    vector<string>vaec = batch.populate(it->second);
+                    //TODO: execute line by line
+                }
+            }
+
+            if(batch.files.size() == 0)
+                continue;
+
+            continue;
+        } else
+        {
+            if (!handle_line(conn, r, q))
+                break;
+        }
     }
 }
