d42b3586d6536fd7b7e0aa18419b55747ac960a4
Carlos Carvalho <carloslack@gmail.com>
2013-07-02 15:46:03 -0300

    This is just the directory tree of the next tool, CryptDBLearn. But before starting to work on this one let's first fix some 'import tool' issues.

---
+++ tools/learn/cryptdblearn.cc
@@ -0,0 +1,9 @@
+#include <cryptdblearn.hh>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    //TODO: Implement this
+    cout << "Hello World from " << __FILE__ << endl;
+    return 0;
+}
