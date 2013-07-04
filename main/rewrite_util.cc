8b420750df3cb9f5ef16446e212dc241f6447119
burrows <burrows@mit.edu>
2013-07-04 16:34:28 -0400

    Created a seperate file for DDL handlers. Compiles, but is broken.

--- main/rewrite_util.cc
+++ main/rewrite_util.cc
@@ -1,6 +1,10 @@
 #include <main/rewrite_util.hh>
+#include <main/cdb_rewrite.hh>
+#include <parser/lex_util.hh>
 
 using namespace std;
+
+extern CItemTypesDir itemTypes;
 
 string
 anonymize_table_name(const string &tname,
@@ -11,4 +15,160 @@ anonymize_table_name(const string &tname,
 
     return tm->anonTableName;
 }
+
+// this function should be called at the root of a tree of items
+// that should be rewritten
+// @context defaults to empty string.
+Item *
+rewrite(Item *i, const OLK & constr, Analysis &a, string context)
+{
+    if (context.size()) {
+	context = " for " + context;
+    }
+    RewritePlan * rp = getAssert(a.rewritePlans, i);
+    assert(rp);
+    if (!rp->es_out.contains(constr)) {
+	cerr << "query cannot be supported because " << i << " needs to return " << constr << context << "\n" \
+	     << "BUT it can only return " << rp->es_out << " BECAUSE " << rp->r << "\n";
+	assert(false);
+    }
+    return itemTypes.do_rewrite(i, constr, rp, a);
+}
+
+TABLE_LIST *
+rewrite_table_list(TABLE_LIST *t, Analysis &a)
+{
+    TABLE_LIST * new_t = copy(t);
+
+    // Table name can only be empty when grouping a nested join.
+    assert(t->table_name || t->nested_join);
+    if (t->table_name) {
+        string anon_name = anonymize_table_name(string(t->table_name,
+                                                       t->table_name_length), a);
+        new_t->table_name = make_thd_string(anon_name, &new_t->table_name_length);
+        new_t->alias = make_thd_string(anon_name);
+        new_t->next_local = NULL;
+    }
+
+    return new_t;
+}
+
+SQL_I_List<TABLE_LIST>
+rewrite_table_list(SQL_I_List<TABLE_LIST> tlist, Analysis &a)
+{
+    if (!tlist.elements) {
+	return SQL_I_List<TABLE_LIST>();
+    }
+    TABLE_LIST * tl = rewrite_table_list(tlist.first, a);
+
+    SQL_I_List<TABLE_LIST> * new_tlist = oneElemList<TABLE_LIST>(tl);
+
+    TABLE_LIST * prev = tl;
+    for (TABLE_LIST *tbl = tlist.first->next_local;
+	 tbl; tbl = tbl->next_local) {
+	TABLE_LIST * new_tbl = rewrite_table_list(tbl, a);
+	prev->next_local = new_tbl;
+	prev = new_tbl;
+    }
+    prev->next_local = NULL;
+
+    return *new_tlist;
+}
+
+List<TABLE_LIST>
+rewrite_table_list(List<TABLE_LIST> tll, Analysis & a)
+{
+    List<TABLE_LIST> * new_tll = new List<TABLE_LIST>();
+
+    List_iterator<TABLE_LIST> join_it(tll);
+
+    for (;;) {
+        TABLE_LIST *t = join_it++;
+        if (!t) {
+	    break;
+	}
+
+        TABLE_LIST * new_t = rewrite_table_list(t, a);
+	new_tll->push_back(new_t);
+
+        if (t->nested_join) {
+            new_t->nested_join->join_list = rewrite_table_list(t->nested_join->join_list, a);
+            return *new_tll;
+        }
+
+        if (t->on_expr) {
+            new_t->on_expr = rewrite(t->on_expr, PLAIN_OLK, a);
+	}
+
+	/* TODO: derived tables
+        if (t->derived) {
+            st_select_lex_unit *u = t->derived;
+            rewrite_select_lex(u->first_select(), a);
+        }
+	*/
+    }
+
+    return *new_tll;
+}
+
+/*
+ * Helper functions to look up via directory & invoke method.
+ */
+RewritePlan *
+gather(Item *i, reason &tr, Analysis & a)
+{
+    return itemTypes.do_gather(i, tr, a);
+}
+
+//TODO: need to check somewhere that plain is returned
+//TODO: Put in gather helpers file.
+void
+analyze(Item *i, Analysis & a)
+{
+    assert(i != NULL);
+    LOG(cdb_v) << "calling gather for item " << *i;
+    reason r;
+    a.rewritePlans[i] = gather(i, r, a);
+}
+
+LEX *
+begin_transaction_lex(Analysis a) {
+    string query = "START TRANSACTION;";
+    query_parse *begin_parse = new query_parse(a.ps->conn->getCurDBName(),
+                                               query);
+    return begin_parse->lex();
+}
+
+LEX *
+commit_transaction_lex(Analysis a) {
+    string query = "COMMIT;";
+    query_parse *commit_parse = new query_parse(a.ps->conn->getCurDBName(),
+                                                query);
+    return commit_parse->lex();
+}
+
+// FIXME(burrows): Generalize to support any container with next AND end
+// semantics.
+template <typename T>
+std::string vector_join(std::vector<T> v, std::string delim,
+                        std::string (*finalize)(T))
+{
+    std::string accum;
+    for (typename std::vector<T>::iterator it = v.begin();
+         it != v.end(); ++it) {
+        std::string element = (*finalize)((T)*it);
+        accum.append(element);
+        accum.append(delim);
+    }
+
+    std::string output;
+    if (accum.length() > 0) {
+        output = accum.substr(0, accum.length() - delim.length());
+    } else {
+        output = accum;
+    }
+
+    return output;
+}
+
 
--- main/rewrite_util.hh
+++ main/rewrite_util.hh
@@ -4,8 +4,39 @@
 
 #include <main/Analysis.hh>
 
+#include <sql_list.h>
+#include <sql_table.h>
+
 // anonymizes table name based on the information in a.schema
 // TODO(burrows): Do we want to handle aliasing here, or up a level?
 std::string
 anonymize_table_name(const std::string &tname,
                      Analysis & a);
+
+Item *
+rewrite(Item *i, const OLK & constr, Analysis &a, string context = "");
+
+TABLE_LIST *
+rewrite_table_list(TABLE_LIST *t, Analysis &a);
+
+SQL_I_List<TABLE_LIST>
+rewrite_table_list(SQL_I_List<TABLE_LIST> tlist, Analysis &a);
+
+List<TABLE_LIST>
+rewrite_table_list(List<TABLE_LIST> tll, Analysis & a);
+
+RewritePlan *
+gather(Item *i, reason &tr, Analysis & a);
+
+void
+analyze(Item *i, Analysis & a);
+
+LEX *
+begin_transaction_lex(Analysis a);
+
+LEX *
+commit_transaction_lex(Analysis a);
+
+template <typename T>
+std::string vector_join(std::vector<T> v, std::string delim,
+                        std::string (*finalize)(T));
