fc12e9bbcdfc25483c3bf5fe52520321d24f70c8
Carlos Carvalho <carloslack@gmail.com>
2013-07-17 23:00:17 -0300

    Signed int works for less than, equal to and greater than. lt, gt and eq does not work yet if we use values below zero for comparing.

--- main/CryptoHandlers.cc
+++ main/CryptoHandlers.cc
@@ -398,11 +398,7 @@ RND_int::decryptUDF(Item * col, Item * ivcol) {
     Item * udfdec = new Item_func_udf_int(&u_decRNDInt, l);
     udfdec->name = NULL; //no alias
 
-    //add encompassing CAST for unsigned
-    Item * udf = new Item_func_unsigned(udfdec);
-    udf->name = NULL;
-
-    return udf;
+    return udfdec;
 }
 
 ///////////////////////////////////////////////
@@ -651,11 +647,7 @@ DET_int::decryptUDF(Item * col, Item * ivcol) {
     Item * udfdec = new Item_func_udf_int(&u_decDETInt, l);
     udfdec->name = NULL;
 
-    //add encompassing CAST for unsigned
-    Item * udf = new Item_func_unsigned(udfdec);
-    udf->name = NULL;
-
-    return udf;
+    return udfdec;
 }
 
 DET_dec::DET_dec(Create_field * cf, const string & seed_key) : DET_int(cf, seed_key) {
