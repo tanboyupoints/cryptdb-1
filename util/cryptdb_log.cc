7eb8667155500a7ef83beb99b671768407ddbcaa
burrows <burrows@mit.edu>
2013-07-31 15:35:51 -0400

    Added mask for getting additional test output.

--- util/cryptdb_log.cc
+++ util/cryptdb_log.cc
@@ -7,5 +7,6 @@ uint64_t cryptdb_logger::enable_mask = //0;
     //   cryptdb_logger::mask(log_group::log_encl) |
 //    cryptdb_logger::mask(log_group::log_edb_v) |
     //cryptdb_logger::mask(log_group::log_am_v) |
+    // cryptdb_logger::mask(log_group::log_test) |
     cryptdb_logger::mask(log_group::log_warn);
 
