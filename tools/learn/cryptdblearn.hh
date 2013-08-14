a53c3c56085b7e30eb474625f8eac47dd9b20866
Carlos Carvalho <carloslack@gmail.com>
2013-08-14 16:35:38 -0300

    Removed old code

--- tools/learn/cryptdblearn.hh
+++ tools/learn/cryptdblearn.hh
@@ -33,21 +33,9 @@ class Learn
         Learn(mode_e mode, ProxyState& ps, const std::string &dbname,
               const std::string &filename)
             : m_totalnum(0), m_success_num(0), m_errnum(0),
-            m_mode(mode), m_ps(ps), m_dbname(dbname), m_filename(filename)
-        {
-            //TODO: setenv is just a sugestion took from test, but need to be
-            //better than this.
-            //We must somehow inform CryptDB that we are in training mode for security
-            //and performance matters, if necessary.
-            setenv("TRAIN_QUERY", modes[m_mode].envname, 1);
-        }
+            m_mode(mode), m_ps(ps), m_dbname(dbname), m_filename(filename){}
 
-        ~Learn(){
-            unsetenv("TRAIN_QUERY");
-            //cout << "Executed queries: " << m_totalnum << endl;
-            //cout << "Errors: " << m_errnum << endl;
-            //TODO: Generate onions enc. layout
-        }
+        ~Learn(){}
 
         void trainFromFile(ProxyState &ps);
         void trainFromScratch(ProxyState &ps);
