379842dc9316e3760c9bd9471b03e204112dbe13
burrows <burrows.labs@gmail.com>
2013-09-04 22:48:07 -0400

    Added util/errstream.hh

---
+++ util/errstream.hh
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+class CryptDBError {
+public:
+    CryptDBError(const std::string &m) : msg(m) {}
+    std::string msg;
+};
+
+class fatal : public std::stringstream {
+ public:
+    ~fatal() __attribute__((noreturn)) {
+        std::cerr << str() << std::endl;
+        exit(-1);
+    }
+};
+
+class cryptdb_err : public std::stringstream {
+ public:
+    ~cryptdb_err() /*throw (CryptDBError)*/ {
+        std::cerr << str() << std::endl;
+        throw CryptDBError(str());
+    }
+};
+
+class thrower : public std::stringstream {
+ public:
+    ~thrower() __attribute__((noreturn)) {
+        throw std::runtime_error(str());
+    }
+};
+
