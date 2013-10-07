37f31058cef20d1096e9dcf73d24203f6489e288
burrows <burrows.labs@gmail.com>
2013-10-06 22:21:32 -0400

    A script that users may find useful for refreshing a cryptdb installation.

---
+++ scripts/refresh.sh
@@ -0,0 +1,6 @@
+rm -r /var/lib/shadow-mysql/*
+mysql -u root -pletmein -e "drop database if exists remote_db"
+mysql -u root -pletmein -e "drop database if exists cryptdbtest"
+mysql -u root -pletmein -e "create database cryptdbtest"
+mysql -u root -pletmein -e "drop database if exists cryptdbtest_control"
+mysql -u root -pletmein -e "create database cryptdbtest_control"
