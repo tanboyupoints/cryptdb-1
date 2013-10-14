9216bbce5aa25159e669f68f110ce4c47855ebd5
burrows <burrows.labs@gmail.com>
2013-10-14 12:00:22 -0400

    installation script doesn't call 'sudo'; generates conf/config.mk; creates shadow directory in cryptdb directory.

--- scripts/install.rb
+++ scripts/install.rb
@@ -6,7 +6,7 @@ require 'fileutils'
 $usage =
     "Usage: ./install <path-to-cryptdb> [automake-version] [gcc-version]"
 
-SHADOW_PATH = "/var/lib/shadow-mysql"
+SHADOW_NAME = "shadow"
 PROXY_NAME = "proxy-src"
 MYSQL_NAME = "mysql-src"
 TAR_GZ = ".tar.gz"
@@ -16,23 +16,28 @@ class String
         "\033[36m#{self}\033[0m"
     end
 
+    def red
+        "\033[31m#{self}\033[0m"
+    end
+
     def bold
         "\033[1m#{self}\033[22m"
     end
 end
 
+# NOTE: For whatever reason we fail to get the correct exit code when
+# apt-get fails; and execution continues per-normal.
 def get_pkgs
     p_puts "Retrieving packages..."
 
     pkg_shell = ShellDoer.new("~")
-    pkg_shell.pretty_execute(%q{
-        sudo apt-get install liblua5.1-0-dev libntl-dev                \
-                             libmysqlclient-dev libssl-dev libbsd-dev  \
-                             libevent-dev libglib2.0-dev libgmp-dev    \
-                             mysql-server libaio-dev automake          \
-                             gtk-doc-tools flex cmake libncurses5-dev  \
-                             bison g++ make                            \
-        1>&2
+    pkg_shell.>(%q{
+        apt-get install liblua5.1-0-dev libntl-dev        \
+                libmysqlclient-dev libssl-dev libbsd-dev  \
+                libevent-dev libglib2.0-dev libgmp-dev    \
+                mysql-server libaio-dev automake          \
+                gtk-doc-tools flex cmake libncurses5-dev  \
+                bison g++ make
     })
 end
 
@@ -63,7 +68,7 @@ def fn(cdb_path, in_make_v=nil, in_gcc_v=nil)
     # untar
     proxy_path = File.join(cryptdb_path, PROXY_NAME)
     proxy_tar_path = File.join(bins_path, PROXY_NAME) + TAR_GZ
-    cryptdb_shell.pretty_execute("tar zxf #{proxy_tar_path}") 
+    cryptdb_shell.>("tar zxf #{proxy_tar_path}") 
 
     # automake compatibility fix
     # https://www.flameeyes.eu/autotools-mythbuster/forwardporting/automake.html
@@ -77,12 +82,12 @@ def fn(cdb_path, in_make_v=nil, in_gcc_v=nil)
         FileUtils.copy(little, config_path)
     end
 
-    mp_shell.pretty_execute("./autogen.sh")
-    mp_shell.pretty_execute("./configure --enable-maintainer-mode --with-lua=lua5.1")
-    mp_shell.pretty_execute("make")
-    mp_shell.pretty_execute("sudo make install")
+    mp_shell.>("./autogen.sh")
+    mp_shell.>("./configure --enable-maintainer-mode --with-lua=lua5.1")
+    mp_shell.>("make")
+    mp_shell.>("make install")
     File.delete(config_path)
-    mp_shell.pretty_execute("rm -rf #{proxy_path}")
+    mp_shell.>("rm -rf #{proxy_path}")
 
     #############################
     #            gcc
@@ -113,25 +118,33 @@ def fn(cdb_path, in_make_v=nil, in_gcc_v=nil)
     # untar
     mysql_path = File.join(cryptdb_path, MYSQL_NAME)
     mysql_tar_path = File.join(bins_path, MYSQL_NAME) + TAR_GZ
-    cryptdb_shell.pretty_execute("tar zxf #{mysql_tar_path}")
+    cryptdb_shell.>("tar zxf #{mysql_tar_path}")
 
     mysql_build_path = File.join(mysql_path, "/build")
     Dir.mkdir(mysql_build_path) if false == File.exists?(mysql_build_path)
 
     mysql_shell = ShellDoer.new(mysql_build_path)
-    mysql_shell.pretty_execute("cmake -DWITH_EMBEDDED_SERVER=on -DENABLE_DTRACE=off ..")
-    mysql_shell.pretty_execute("make")
+    mysql_shell.>("cmake -DWITH_EMBEDDED_SERVER=on -DENABLE_DTRACE=off ..")
+    mysql_shell.>("make")
 
     #############################
     #          cryptdb
     #############################
     p_puts "Building cryptdb..."
 
-    cryptdb_shell.pretty_execute("make MYSRC=#{mysql_path}")
-    cryptdb_shell.pretty_execute("sudo make install MYSRC=#{mysql_path}")
-    cryptdb_shell.pretty_execute("sudo rm -rf #{SHADOW_PATH}")
-    cryptdb_shell.pretty_execute("sudo mkdir #{SHADOW_PATH}")
-    cryptdb_shell.pretty_execute("sudo chown #{Etc.getlogin} #{SHADOW_PATH}")
+    conf_path = File.join(cryptdb_path, "conf", "config.mk")
+    File.open(conf_path, 'w') do |file|
+        file.write(generate_config(mysql_path))
+    end
+    cryptdb_shell.>("make")
+    cryptdb_shell.>("make install")
+
+    shadow_path = File.join(cryptdb_path, SHADOW_NAME)
+    cryptdb_shell.>("rm -rf #{shadow_path}")
+    cryptdb_shell.>("mkdir #{shadow_path}")
+
+    # Give the user access to all the stuff we created.
+    cryptdb_shell.>("chown -R #{Etc.getlogin} #{cryptdb_path}")
 end
 
 class ShellDoer
@@ -139,10 +152,15 @@ class ShellDoer
         @dir = dir
     end
 
+    def >(cmd)
+        pretty_execute(cmd)
+    end
+
+    private
     def pretty_execute(cmd)
         %x(cd #{@dir} && #{cmd} 1>&2)
         if $?.exitstatus != 0
-            fail "`#{cmd}` failed"
+            fail "`#{cmd}` failed".red.bold
         end
     end
 end
@@ -197,6 +215,20 @@ def p_puts(output_me)
     puts output_me.cyan.bold
 end
 
+def generate_config(mysql_path)
+    ["MYSRC := #{mysql_path}",
+     "MYBUILD  := $(MYSRC)/build",
+     "",
+     "## To enable debugging:",
+     "# CXXFLAGS += -g",
+     "",
+     "## To use a different compiler:",
+     "# CXX := g++-4.6",
+     "",
+     "## Where UDFs go:",
+     "MYSQL_PLUGIN_DIR := /usr/lib/mysql/plugin"].join("\n")
+end
+
 def test_version
     pairs = [["1.1",        "1.3"],
              ["1.5",        "2.5"],
@@ -205,13 +237,19 @@ def test_version
              ["1.0.0.1",    "1.1"],
              ["3.4.5",      "4.5.2"],
              ["2.0",        "5.1.0.0"],
-             ["0.1", "0.1.0.0.2"],
-             ["0", "0.1"]]
+             ["0.1",        "0.1.0.0.2"],
+             ["0",          "0.1"]]
 
     pairs.inject(true) do |acc, (low, high)|
         Version.new(low) < Version.new(high) &&
         Version.new(high) >= Version.new(low) &&
         acc
+    end
+end
+
+def root?
+    if 0 != Process.uid
+        fail "root required to install dependencies and mysql-proxy".red.bold
     end
 end
 
@@ -224,6 +262,7 @@ if ARGV.size() < 1 || ARGV.size() > 3
     fail $usage
 end
 
+root?()
 get_pkgs()
 fn(ARGV[0], ARGV[1], ARGV[2])
 
