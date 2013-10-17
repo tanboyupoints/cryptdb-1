390dc5efa6ad846b710f4f72b0148c7b14041933
burrows <burrows.labs@gmail.com>
2013-10-17 06:10:49 -0400

    bug fixes for double proxy; need to make comparison work.

--- scripts/double.lua
+++ scripts/double.lua
@@ -1,43 +1,88 @@
-luasql = require "luasql.mysql"
+package.cpath = package.cpath .. ";/usr/local/lib/lua/5.1/?.so"
+local luasql = assert(require("luasql.mysql"))
+local proto = assert(require("mysql.proto"))
+
 local LOG_FILE = "logs/double.log"
+local env = nil
+local cryptdb_connection = nil
 
 function connect_server()
+    print("Double Connection.")
     -- initialize cryptdb connection
-    env = assert(luasql.mysql())
-    connection = assert(env:connect("", "root", "letmein", "127.0.0.1", 3307))
+    env = luasql.mysql()
+    if env then
+        cryptdb_connection = env:connect("", "root", "letmein", "127.0.0.1", 3307)
+    end
 
     -- open log file
-    assert(io.output(LOG_FILE))
+    io.output(LOG_FILE)
+end
+
+function disconnect_client()
+    if cryptdb_connection then
+        cryptdb_connection:close()
+    end
+
+    if env then
+        env:close()
+    end
+end
+
+function read_query(packet)
+    local query = string.sub(packet, 2)
+    if string.byte(packet) == proxy.COM_INIT_DB then
+        query = "USE " .. query
+    end
+    packet = string.char(proxy.COM_QUERY) .. query
+
+    proxy.queries:append(42, packet, {resultset_is_needed = true})
+    return proxy.PROXY_SEND_QUERY
 end
 
 function read_query_result(inj)
     local client_name = proxy.connection.client.src.name
+    local query = string.sub(inj.query, 2)
+
+    proxy.response.type          = proxy.MYSQLD_PACKET_OK
+    proxy.response.affected_rows = inj.resultset.affected_rows
+    proxy.response.insert_id     = inj.resultset.insert_id
+
+    if nil == cryptdb_connection then
+        io.write(create_log_entry(client_name, query, false, false,
+                                  "no cryptb connection"))
+        io.flush()
+        return proxy.PROXY_SEND_RESULT
+    end
 
     -- > somemtimes this is a cursor (ie SELECT), sometimes it's nil (query
-    --   error), sometimes it's a number indicating the number of rows.
+    --   error), sometimes it's number of rows affected by command
     local out_status = nil
-    local cursor = env:execute(inj.query)
-    local cryptdb_error = nil == cursor
-    local regular_error = proxy.MYSQLD_PACKET_ERR == inj.resultset.query_status
+    cursor = assert(cryptdb_connection:execute(query))
+    local cryptdb_error = not cursor
+    local regular_error
+    if proxy.MYSQLD_PACKET_ERR == inj.resultset.query_status then
+        regular_error = true
+    else
+        regular_error = false
+    end
+
     if cryptdb_error or regular_error then
+        proxy.response.type = proxy.MYSQLD_PACKET_ERR
+        local err = proto.from_err_packet(inj.resultset.raw)
+        proxy.response.errmsg = err.msg
+        proxy.response.errcode = err.errcode
+        proxy.response.sqlstate = err.sqlstate
+
         out_status = "error"
     elseif "number" == type(cursor) then
-        if cursor ~= 0 then
-            out_status = "missing cryptdb results"
-        else
-            -- hopefully the size is zero, otherwise this is slow.
-            local zero = true
-            for row in inj.resultset.rows do
-                zero = false
-                break
-            end
-            out_status = get_match_text(zero)
-        end
+        -- WARN: this is always going to give the wrong result
+        -- for UPDATE.
+        out_status = get_match_text(cursor == inj.resultset.affected_rows)
     else
         -- do the naive comparison while gathering the results,
         -- then if it fails, do the slow comparison
 
-        local cryptdb_fields = cur:getcolnames()
+        local cryptdb_fields = cursor:getcolnames()
         local regular_fields = {}
         for i = 1, #inj.resultset.fields do
             regular_fields[i] = inj.resultset.fields[i]
@@ -55,10 +100,13 @@ function read_query_result(inj)
             for regular_row in inj.resultset.rows do
                 -- will be nil after last
                 local cryptdb_row = cursor:fetch(cryptdb_results, "n")
+                -- FIXME: This comparison is suspect as well.
                 if nil == cryptdb_row or cryptdb_row ~= regular_row then
-                    return create_log_entry(client_name, inj.query,
-                                            cryptdb_error, regular_error,
-                                            get_match_text(false))
+                    io.write(create_log_entry(client_name, query,
+                                              cryptdb_error, regular_error,
+                                              get_match_text(false)))
+                    io.flush()
+                    return proxy.PROXY_SEND_RESULT
                 end
 
                 regular_results[index] = regular_row
@@ -79,7 +127,7 @@ function read_query_result(inj)
         end
     end
 
-    io.write(create_log_entry(client_name, inj.query, cryptdb_error,
+    io.write(create_log_entry(client_name, query, cryptdb_error,
                               regular_error, out_status))
     io.flush()
 
@@ -123,11 +171,6 @@ function slow_test(results_a, results_b)
     end
 
     return true
-end
-
-function cleanup()
-    connection:close()
-    env:close()
 end
 
 function get_match_text(b)
