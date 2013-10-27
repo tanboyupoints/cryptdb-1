c4362e3bc98740c7ae598c37e90d711b27851c5b
burrows <burrows.labs@gmail.com>
2013-10-26 23:31:18 -0400

    no references to old shadow directory.

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,4 +1,3 @@
-sudo rm -rf /var/lib/shadow-mysql/*
 rm -rf $EDBDIR/shadow/*
 mysql -u root -pletmein -e "drop database if exists remote_db"
 mysql -u root -pletmein -e "drop database if exists cryptdbtest"
