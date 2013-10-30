062f802b3d7da921c6a37a2b46c41985ea2a0c73
burrows <burrows.labs@gmail.com>
2013-10-30 06:37:32 -0400

    Thread now waits for input from another function call.

--- scripts/threaded_query.c
+++ scripts/threaded_query.c
@@ -3,8 +3,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <string.h>
 
 #include <pthread.h>
+#include <sys/types.h>
 
 #include <mysql/mysql.h>
 
@@ -12,8 +14,13 @@
 #include <lua5.1/lauxlib.h>
 #include <lua5.1/lualib.h>
 
-int
-do_query(lua_State *const L, const char *const query);
+struct LuaQuery {
+    const char *query;      // remove
+    void **magic_ptr;
+};
+
+void *
+do_query(void *lq);
 
 static int
 pass_table(lua_State *const L)
@@ -45,21 +52,91 @@ pushvalue(lua_State *const L, void *const row, long int len)
 static int
 query(lua_State *const L)
 {
-    // FIXME: Get real query.
-    const char *const query = "SELECT * FROM lua.t";
+    size_t length;
+    // FIXME:
+    char *const query = calloc(200, sizeof(char));
+    strcpy(query, lua_tolstring(L, 1, &length));
+
+    struct LuaQuery *const lua_query = malloc(sizeof(struct LuaQuery));
+    if (!lua_query) {
+        fprintf(stderr, "malloc failed!\n");
+        lua_pushboolean(L, false);
+        lua_pushnil(L);
+        lua_pushnil(L);
+        return 3;
+    }
+
+    lua_query->magic_ptr = malloc(sizeof(void *));
+    if (!lua_query->magic_ptr) {
+        free(lua_query);
+        fprintf(stderr, "malloc failed!\n");
+        lua_pushboolean(L, false);
+        lua_pushnil(L);
+        lua_pushnil(L);
+        return 3;
+    }
+    *lua_query->magic_ptr = NULL;
+    lua_query->query = query;
+
+    pthread_t thread;
     // const char *const query = "CREATE TABLE lua.something6(x integer)";
-    return do_query(L, query);
+    if (pthread_create(&thread, NULL, do_query, (void *)lua_query)) {
+        free(lua_query->magic_ptr);
+        free(lua_query);
+        fprintf(stderr, "pthread_create failed!\n");
+        lua_pushboolean(L, false);
+        lua_pushnil(L);
+        lua_pushnil(L);
+        return 3;
+    }
+
+    lua_pushboolean(L, true);
+    lua_pushnumber(L, (unsigned long)lua_query);
+    lua_pushnumber(L, thread);
+    return 3;
 }
 
-int
-do_query(lua_State *const L, const char *const query)
+static int
+results(lua_State *const L)
 {
-    MYSQL *const init = mysql_init(NULL);
-    if (!init) {
-        fprintf(stderr, "mysql_init failed!\n");
+    struct LuaQuery *const lua_query =
+        (struct LuaQuery *)lua_tointeger(L, 1);
+    pthread_t thread = (pthread_t)lua_tointeger(L, 2);
+
+    *lua_query->magic_ptr = L;
+
+    int thread_output;
+    if (pthread_join(thread, (void **)&thread_output)) {
+        free(lua_query->magic_ptr);
+        free(lua_query);
+        fprintf(stderr, "pthread_join failed!\n");
         lua_pushboolean(L, false);
         lua_pushnil(L);
         return 2;
+    }
+
+    printf("thread out: %d\n", thread_output);
+    free(lua_query->magic_ptr);
+    free(lua_query);
+    return thread_output;
+}
+
+static lua_State *
+handleMagicPointer(struct LuaQuery *lua_query)
+{
+    while (NULL == *lua_query->magic_ptr);
+    return (lua_State *)*lua_query->magic_ptr;
+}
+
+void *
+do_query(void *lq)
+{
+    struct LuaQuery *lua_query = (struct LuaQuery *)lq;
+    void **const magic_ptr = lua_query->magic_ptr;
+    MYSQL *const init = mysql_init(NULL);
+    if (!init) {
+        fprintf(stderr, "mysql_init failed!\n");
+        goto error_exit;
     }
 
     const char *const host      = "localhost";
@@ -71,17 +148,13 @@ do_query(lua_State *const L, const char *const query)
         mysql_real_connect(init, host, user, passwd, NULL, port, NULL, 0);
     if (!conn) {
         fprintf(stderr, "mysql_real_connect failed!\n");
-        lua_pushboolean(L, false);
-        lua_pushnil(L);
-        return 2;
+        goto error_exit;
     }
 
-    if (mysql_query(conn, query)) {
+    if (mysql_query(conn, lua_query->query)) {
         mysql_close(conn);
         fprintf(stderr, "mysql_query failed!\n");
-        lua_pushboolean(L, false);
-        lua_pushnil(L);
-        return 2;
+        goto error_exit;
     }
 
     MYSQL_RES *const result = mysql_store_result(conn);
@@ -90,43 +163,53 @@ do_query(lua_State *const L, const char *const query)
         if (0 == field_count) {
             assert(0 == mysql_errno(conn));
 
-            lua_pushboolean(L, true);
-            lua_pushnumber(L, mysql_affected_rows(conn));
-            return 2;
+            lua_State *const ell = handleMagicPointer(lua_query);
+            lua_pushboolean(ell, true);
+            lua_pushnumber(ell, mysql_affected_rows(conn));
+            return (void *)2;
         }
 
         assert(mysql_errno(conn));
+        mysql_close(conn);
         fprintf(stderr, "mysql_store_result failed!\n");
-        lua_pushboolean(L, false);
-        lua_pushnil(L);
-        return 2;
+        goto error_exit;
     }
     assert(0 == mysql_errno(conn));
 
+    lua_State *const ell = handleMagicPointer(lua_query);
     // resultset
-    lua_pushboolean(L, true);
-    lua_newtable(L);
-
+    lua_pushboolean(ell, true);
+    lua_newtable(ell);
     MYSQL_ROW row;
     int row_index = 0;
     while ((row = mysql_fetch_row(result))) {
         unsigned long *const l = mysql_fetch_lengths(result);
 
         // row
-        lua_pushnumber(L, row_index + 1);
-        lua_newtable(L);
+        lua_pushnumber(ell, row_index + 1);
+        lua_newtable(ell);
         int col_index = 0;
         for (; col_index < field_count; ++col_index) {
-            lua_pushnumber(L, col_index + 1);
-            pushvalue(L, row[col_index], l[col_index]);
-            lua_settable(L, -3);
+            lua_pushnumber(ell, col_index + 1);
+            pushvalue(ell, row[col_index], l[col_index]);
+            lua_settable(ell, -3);
         }
-        lua_settable(L, -3);
+        lua_settable(ell, -3);
 
         row_index += 1;
     }
 
-    return 2;
+    return (void *)2;
+
+error_exit:
+    fprintf(stderr, "error exit!\n");
+    ;
+    lua_State *const ell_err = handleMagicPointer(lua_query);
+
+    lua_pushboolean(ell_err, false);
+    lua_pushnil(ell_err);
+
+    return (void *)2;
 }
 
 static const struct luaL_reg
@@ -134,6 +217,7 @@ main_lib[] = {
 #define F(n) { #n, n }
     F(pass_table),
     F(query),
+    F(results),
     {0, 0},
 };
 
