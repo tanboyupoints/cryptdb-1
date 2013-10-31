abd96c792b251e900cba56011116891d6c672187
burrows <burrows.labs@gmail.com>
2013-10-31 07:33:27 -0400

    double proxy no longer deadlocks.

--- scripts/double.lua
+++ scripts/double.lua
@@ -7,27 +7,21 @@ local new_session       = true            -- wordpress testing hack
 local cryptdb_is_dead   = false
 local result_set_count  = nil
 local result_set_index  = nil
-local swallowed         = false
 
 package.cpath = package.cpath .. ";/usr/local/lib/lua/5.1/?.so"
 package.path  = package.path .. ";/usr/local/share/lua/5.1/?.lua"
 
 local CRYPTDB_DIR = os.getenv("EDBDIR")
-local get_locklib =
-    assert(package.loadlib(CRYPTDB_DIR .. "/obj/scripts/locklib.so",
-                           "luaopen_locklib"))
-get_locklib()
--- local luasql = assert(require("luasql.mysql"))
-local proto  = assert(require("mysql.proto"))
-local lanes  = assert(require("lanes"))
-if require("lanes").configure then
-    lanes.configure()
-end
-local linda  = lanes.linda()
 
-local LOCK_NAME          = "cdb.lock"
+local threaded_query =
+    assert(package.loadlib(CRYPTDB_DIR .. "/scripts/threaded_query.so",
+                           "lua_main_init"))
+threaded_query()
+
+local proto  = assert(require("mysql.proto"))
+
 local LOG_FILE_PATH      = CRYPTDB_DIR .. "/logs/double.log"
-local cryptdb_lane       = nil
+local tquery             = nil
 local log_file_h         = nil
 
 local RESULTS_QUEUE      = "results"
@@ -39,10 +33,12 @@ local CRYPTDB_IS_DEAD    = "cryptdb is dead"
 
 function connect_server()
     print("Double Connection.")
+
     -- initialize and start pre-emptive thread
-    local cryptdb_lane_gen =
-        lanes.gen("*", {required = {'luasql.mysql',}}, exec_q)
-    cryptdb_lane = cryptdb_lane_gen()
+    status, tquery = ThreadedQuery.start()
+    if nil == status then
+        tquery = nil
+    end
 
     -- open log file
     log_file_h = assert(io.open(LOG_FILE_PATH, "a"))
@@ -53,36 +49,20 @@ function disconnect_client()
         log_file_h:close()
     end
 
-    assert(cryptdb_lane:cancel(5.0))
+    if tquery then
+        assert(ThreadedQuery.kill(tquery))
+    end
 end
 
 function read_query(packet)
     result_set_count = 0
     result_set_index = 0
-    swallowed        = false
 
-    local query  = string.sub(packet, 2)
+    local query = string.sub(packet, 2)
 
-    -- don't acquire lock or do anything with cryptdb if the query is blank
-    -- > must be handled seperately because a blank query doesn't properly
-    -- trigger 'read_query_result(...)'
-    if 0 == string.len(query:gsub("%s+", "")) then
+    -- bad queries don't properly trigger read_query_result
+    if not valid_packet(packet) then
         return nil
-    end
-
-    -- acquire lock if cryptdb is functional
-    if false == cryptdb_is_dead then
-        status, lock_fd = locklib.acquire_lock(LOCK_NAME)
-        if false == status then
-            print("Swallowed Query: [" ..  query .. "]")
-            local noop = "do 0;"
-            proxy.queries:append(13, string.char(proxy.COM_QUERY) .. noop,
-                                 {resultset_is_needed = true})
-            result_set_count = result_set_count + 1
-            swallowed        = true
-            return proxy.PROXY_SEND_QUERY
-        end
-        assert(lock_fd)
     end
 
     -- HACK: turns off strict mode for wordpress because
@@ -108,19 +88,20 @@ function read_query(packet)
             cryptdb_query = query
         end
 
-        -- Clear the queues
-        -- linda:set(QUERY_QUEUE)
-        linda:set(RESULTS_QUEUE)
-
         -- Send the new query
-        linda:send(QUERY_QUEUE, cryptdb_query)
+        if tquery then
+            ThreadedQuery.query(tquery, cryptdb_query);
+        end
     end
 
     return proxy.PROXY_SEND_QUERY
 end
 
 function read_query_result(inj)
-    assert(not not swallowed ~= not not lock_fd)
+    result_set_index = result_set_index + 1
+    if result_set_index < result_set_count then
+        return proxy.PROXY_IGNORE_RESULT
+    end
 
     local client_name = proxy.connection.client.src.name
     local query = string.sub(inj.query, 2)
@@ -134,26 +115,31 @@ function read_query_result(inj)
 
     -- > somemtimes this is a table (ie SELECT), sometimes it's nil (query
     --   error), sometimes it's number of rows affected by command
-    key, cryptdb_results = linda:receive(1.0, RESULTS_QUEUE)
-    local cryptdb_error = not cryptdb_results
+    if result_set_count == result_set_index then
+        if tquery then
+            status, cryptdb_results = ThreadedQuery.results(tquery)
+        else
+            status, cryptdb_results = nil, {}
+        end
+    else
+        status = true
+        cryptdb_results = {}
+        return
+    end
+
+    local cryptdb_error = not status
     local regular_error = proxy.MYSQLD_PACKET_ERR ==
                                 inj.resultset.query_status
 
     if regular_error or cryptdb_error then
         out_status = "error"
-    elseif "string" == type(cryptdb_results) then
-        -- special message
-        if CRYPTDB_IS_DEAD == cryptdb_results then
-            cryptdb_is_dead = true
-            out_status = "cryptdb is dead"
-        end
     elseif "number" == type(cryptdb_results) then
         -- WARN: this is always going to give a nonsensical result
         -- for UPDATE and DDL queries.
         out_status =
             get_match_text(cryptdb_results == inj.resultset.affected_rows)
-    elseif nil == inj.resultset.rows then
-        out_status = "no result data from regular db"
+    elseif not inj.resultset or not inj.resultset.rows then
+        out_status = "no plaintext data"
     else
         -- do the naive comparison while gathering regular results,
         -- then if it fails, do the slow comparison
@@ -189,77 +175,7 @@ function read_query_result(inj)
     create_log_entry(log_file_h, client_name, query, cryptdb_error,
                      regular_error, out_status)
 
-    -- release lock
-    assert(lock_fd)
-    result_set_index = result_set_index + 1
-    if result_set_index == result_set_count then
-        locklib.release_lock(lock_fd)
-        lock_fd = nil
-        return
-    end
-
-    return proxy.PROXY_IGNORE_RESULT
-end
-
--- never returns (sends messages)
-function exec_q()
-    -- init
-    init = function ()
-        local luasql = require("luasql.mysql")
-        if nil == luasql then
-            return nil
-        end
-
-        local env = luasql.mysql()
-        if nil == env then
-            return nil
-        end
-
-        return env:connect("", "root", "letmein", "127.0.0.1", 3307)
-    end
-
-    local c = init()
-    -- failure exit
-    if nil == c then
-        io.stderr:write("\nERROR: failed to connect to cryptdb\n\n")
-        -- once we know the main thread is waiting for query results,
-        -- tell him that we are dead
-        linda:receive(QUERY_QUEUE)
-        linda:send(RESULTS_QUEUE, CRYPTDB_IS_DEAD)
-        return nil
-    end
-
-    set_finalizer(function(err) if c then c:close() end end)
-
-    -- query/parse loop
-    while true do
-        -- get query
-        local key, query = linda:receive(1.0, QUERY_QUEUE)
-
-        if nil ~= query then
-            -- do query
-            local cursor = c:execute(query)
-
-            -- gather results
-            local result = {}
-            if nil == cursor then
-                result = nil
-            elseif "number" == type(cursor) then
-                result = cursor
-            else
-                local row = cursor:fetch({}, "n")
-                local index = 1
-                while row do
-                    result[index] = row
-                    row = cursor:fetch({}, "n")
-
-                    index = index + 1
-                end
-            end
-
-            linda:send(RESULTS_QUEUE, result)
-        end
-    end
+    return
 end
 
 function create_log_entry(file, client, query, cryptdb_error,
@@ -288,8 +204,7 @@ function table_test(results_a, results_b)
     return true
 end
 
--- FIXME: Can get 2x speed up by removing matched elements from the inner
--- lookup array.
+-- FIXME: remove matched elements
 function slow_test(results_a, results_b)
     if table.getn(results_a) ~= table.getn(results_b) then
         return false
@@ -326,6 +241,19 @@ function ppbool(b)
     else
         return "false"
     end
+end
+
+function valid_packet(packet)
+    if not packet:match("[%w%p]+") then
+        return false
+    end
+
+    local opcode = string.byte(packet)
+    if opcode ~= proxy.COM_INIT_DB and opcode ~= proxy.COM_QUERY then
+        return false;
+    end
+
+    return true
 end
 
 -- FIXME: Implement this if it actually matters; will make code slower.
