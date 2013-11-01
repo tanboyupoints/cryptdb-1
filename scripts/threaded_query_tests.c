f3ac47cd8c29d0768c01676f252d49e0e65c07e6
burrows <burrows.labs@gmail.com>
2013-11-01 03:16:36 -0400

    Added simple testing DSL.

--- scripts/threaded_query_tests.c
+++ scripts/threaded_query_tests.c
@@ -1,12 +1,47 @@
 #define COLOR_END       "\033[0m"
 #define GREEN_BEGIN     "\033[1;92m"
+#define RED_BEGIN       "\033[1;31m"
 
-#define TEST_SUCCESS(name)                          \
+
+// ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+// ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+//            Simple Testing DSL
+// ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+// ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+#define DEFINED_NAME
+
+#define TEST_SUCCESS                                \
 {                                                   \
    printf("%s%s succeeded!%s\n", GREEN_BEGIN,       \
-                                (name),             \
+                                (__test_name),      \
                                 COLOR_END);         \
 }
+
+#define TEST_FAILURE                                \
+{                                                   \
+   printf("%s%s failed!%s\n", RED_BEGIN,            \
+                              (__test_name),        \
+                              COLOR_END);           \
+}
+
+#define TEST_ASSERT(expr)                           \
+{                                                   \
+    if (!(expr)) {                                  \
+        TEST_FAILURE                                \
+        return 0;                                   \
+    }                                               \
+}
+
+#define TEST(name)                                  \
+    static int                                      \
+    name(struct lua_State *const L)                 \
+    {                                               \
+        const char *const __test_name = #name;
+
+#define END_TEST                                    \
+        TEST_SUCCESS                                \
+    }
 
 // ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 // ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -14,9 +49,7 @@
 // ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 // ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-static int
-test_pushvalue(struct lua_State *const L)
-{
+TEST(test_pushvalue)
     const char *s0 = "hello";
     const char *s1 = "world\0w0rld"; unsigned int s1_len = 11;
     const char *s2 = "paradise";
@@ -27,20 +60,15 @@ test_pushvalue(struct lua_State *const L)
     pushvalue(L, NULL, 1);
     pushvalue(L, s2, strlen(s2));
 
-    assert(!strcmp(s2, lua_tolstring(L, -1, NULL)));
-    assert(NULL == lua_tolstring(L, -2, NULL));
-    assert(!strcmp(s1, lua_tolstring(L, -3, NULL)));
-    assert(!memcmp(s1, lua_tolstring(L, -4, NULL), s1_len));
-    assert(NULL == lua_tolstring(L, -5, NULL));
-    assert(!strcmp(s0, lua_tolstring(L, -6, NULL)));
+    TEST_ASSERT(!strcmp(s2, lua_tolstring(L, -1, NULL)));
+    TEST_ASSERT(NULL == lua_tolstring(L, -2, NULL));
+    TEST_ASSERT(!strcmp(s1, lua_tolstring(L, -3, NULL)));
+    TEST_ASSERT(!memcmp(s1, lua_tolstring(L, -4, NULL), s1_len));
+    TEST_ASSERT(NULL == lua_tolstring(L, -5, NULL));
+    TEST_ASSERT(!strcmp(s0, lua_tolstring(L, -6, NULL)));
+END_TEST
 
-    TEST_SUCCESS("test_pushvalue");
-    return 0;
-}
-
-static int
-test_luaToCharp(lua_State *const L)
-{
+TEST(test_luaToCharp)
     const unsigned int number = 10;
     const char *const s0      = "red";
     const char *const s1      = "green\0purple"; unsigned s1_len = 11;
@@ -52,35 +80,25 @@ test_luaToCharp(lua_State *const L)
     lua_pushnumber(L, number);
 
     lua_pushstring(L, s2);
-    assert(!strcmp(luaToCharp(L, -1), s2));
+    TEST_ASSERT(!strcmp(luaToCharp(L, -1), s2));
 
     lua_pushstring(L, s0);
-    assert(!strcmp(luaToCharp(L, -1), s0));
+    TEST_ASSERT(!strcmp(luaToCharp(L, -1), s0));
 
-    assert(number == lua_tointeger(L, -3));
-    assert(!strcmp(luaToCharp(L, -4), s1));
-    assert(!memcmp(luaToCharp(L, -5), s1, s1_len));
-    assert(!strcmp(luaToCharp(L, -6), s0));
+    TEST_ASSERT(number == lua_tointeger(L, -3));
+    TEST_ASSERT(!strcmp(luaToCharp(L, -4), s1));
+    TEST_ASSERT(!memcmp(luaToCharp(L, -5), s1, s1_len));
+    TEST_ASSERT(!strcmp(luaToCharp(L, -6), s0));
+END_TEST
 
-    TEST_SUCCESS("test_luaToCharp");
-    return 0;
-}
-
-static int
-test_waitForCommand(struct lua_State *const L)
-{
+TEST(test_waitForCommand)
     struct LuaQuery lua_query;
     lua_query.persist.ell = (void *)0x01;
     lua_query.command_ready = true;
     waitForCommand(&lua_query);
+END_TEST
 
-    TEST_SUCCESS("test_waitForCommand");
-    return 0;
-}
-
-static int
-test_completeLuaQuery(struct lua_State *const L)
-{
+TEST(test_completeLuaQuery)
     const unsigned int init_command     = RESULTS;
     const pthread_t init_thread         = (pthread_t)0x1;
     const void *const init_host_data    = (void *)0x2;
@@ -101,23 +119,18 @@ test_completeLuaQuery(struct lua_State *const L)
     };
 
     completeLuaQuery(&lua_query, 0);
-    assert(init_command         == lua_query.command);
-    assert(false                == lua_query.command_ready);
-    assert(true                 == lua_query.completion_signal);
-    assert(init_thread          == lua_query.thread);
-    assert(init_restarts        == lua_query.persist.restarts);
-    assert(init_host_data       == lua_query.persist.host_data);
-    assert(L                    == lua_query.persist.ell);
-    assert(NULL                 == lua_query.query);
-    assert(COMMAND_OUTPUT_COUNT == lua_query.output_count);
+    TEST_ASSERT(init_command         == lua_query.command);
+    TEST_ASSERT(false                == lua_query.command_ready);
+    TEST_ASSERT(true                 == lua_query.completion_signal);
+    TEST_ASSERT(init_thread          == lua_query.thread);
+    TEST_ASSERT(init_restarts        == lua_query.persist.restarts);
+    TEST_ASSERT(init_host_data       == lua_query.persist.host_data);
+    TEST_ASSERT(L                    == lua_query.persist.ell);
+    TEST_ASSERT(NULL                 == lua_query.query);
+    TEST_ASSERT(COMMAND_OUTPUT_COUNT == lua_query.output_count);
+END_TEST
 
-    TEST_SUCCESS("test_completeLuaQuery");
-    return 0;
-}
-
-static int
-test_newLuaQuery(struct lua_State *const L)
-{
+TEST(test_newLuaQuery)
     struct LuaQuery lua_query;
     memset(&lua_query, 0xDEADBEEF, sizeof(struct LuaQuery));
 
@@ -133,29 +146,24 @@ test_newLuaQuery(struct lua_State *const L)
         .port       = init_port
     };
 
-    assert(newLuaQuery(&lua_query, &host_data));
+    TEST_ASSERT(newLuaQuery(&lua_query, &host_data));
 
-    assert(-1                   == lua_query.command);
-    assert(false                == lua_query.command_ready);
-    assert(false                == lua_query.completion_signal);
-    assert(NO_THREAD            != lua_query.thread);
-    assert(0                    == lua_query.persist.restarts);
-    assert(&host_data           == lua_query.persist.host_data);
-    assert(NULL                 == lua_query.persist.ell);
-    assert(NULL                 == lua_query.query);
-    assert(-1                   == lua_query.output_count);
+    TEST_ASSERT(-1                   == lua_query.command);
+    TEST_ASSERT(false                == lua_query.command_ready);
+    TEST_ASSERT(false                == lua_query.completion_signal);
+    TEST_ASSERT(NO_THREAD            != lua_query.thread);
+    TEST_ASSERT(0                    == lua_query.persist.restarts);
+    TEST_ASSERT(&host_data           == lua_query.persist.host_data);
+    TEST_ASSERT(NULL                 == lua_query.persist.ell);
+    TEST_ASSERT(NULL                 == lua_query.query);
+    TEST_ASSERT(-1                   == lua_query.output_count);
 
     // the thread is using data allocated from this thread and will
     // segfault and shutdown if we do not manually kill.
-    assert(stopLuaQueryThread(&lua_query));
-    TEST_SUCCESS("test_newLuaQuery");
+    TEST_ASSERT(stopLuaQueryThread(&lua_query));
+END_TEST
 
-    return 0;
-}
-
-static int
-test_createLuaQuery(struct lua_State *const L)
-{
+TEST(test_createLuaQuery)
     const char *const init_host     = "127.0.0.1";
     const char *const init_user     = "rainbow";
     const char *const init_passwd   = "jetplane";
@@ -172,45 +180,37 @@ test_createLuaQuery(struct lua_State *const L)
     struct LuaQuery *const lua_query =
         createLuaQuery((struct HostData **)&p_host_data);
 
-    assert(lua_query);
-    assert(-1                   == lua_query->command);
-    assert(false                == lua_query->command_ready);
-    assert(false                == lua_query->completion_signal);
-    assert(NO_THREAD            != lua_query->thread);
-    assert(0                    == lua_query->persist.restarts);
-    assert(&host_data           == lua_query->persist.host_data);
-    assert(NULL                 == lua_query->persist.ell);
-    assert(NULL                 == lua_query->query);
-    assert(-1                   == lua_query->output_count);
+    TEST_ASSERT(lua_query);
+    TEST_ASSERT(-1                   == lua_query->command);
+    TEST_ASSERT(false                == lua_query->command_ready);
+    TEST_ASSERT(false                == lua_query->completion_signal);
+    TEST_ASSERT(NO_THREAD            != lua_query->thread);
+    TEST_ASSERT(0                    == lua_query->persist.restarts);
+    TEST_ASSERT(&host_data           == lua_query->persist.host_data);
+    TEST_ASSERT(NULL                 == lua_query->persist.ell);
+    TEST_ASSERT(NULL                 == lua_query->query);
+    TEST_ASSERT(-1                   == lua_query->output_count);
 
-    assert(NULL                 == p_host_data);
-    assert(host_data.host       == lua_query->persist.host_data->host);
-    assert(host_data.user       == lua_query->persist.host_data->user);
-    assert(host_data.passwd     == lua_query->persist.host_data->passwd);
-    assert(host_data.port       == lua_query->persist.host_data->port);
+    TEST_ASSERT(NULL                 == p_host_data);
+    TEST_ASSERT(host_data.host       == lua_query->persist.host_data->host);
+    TEST_ASSERT(host_data.user       == lua_query->persist.host_data->user);
+    TEST_ASSERT(host_data.passwd     == lua_query->persist.host_data->passwd);
+    TEST_ASSERT(host_data.port       == lua_query->persist.host_data->port);
 
-    assert(stopLuaQueryThread(lua_query));
-    TEST_SUCCESS("test_createLuaQuery");
-}
+    TEST_ASSERT(stopLuaQueryThread(lua_query));
+END_TEST
 
-static int
-test_stopLuaQueryThread(struct lua_State *const L)
-{
+TEST(test_stopLuaQueryThread)
     struct LuaQuery lua_query, test_lua_query;
     memset(&lua_query, 0x13371337, sizeof(struct LuaQuery));
     memcpy(&test_lua_query, &lua_query, sizeof(struct LuaQuery));
 
     test_lua_query.thread = lua_query.thread = (pthread_t)malloc(20);
-    assert(false == stopLuaQueryThread(&lua_query));
-    assert(!memcmp(&lua_query, &test_lua_query, sizeof(struct LuaQuery)));
+    TEST_ASSERT(false == stopLuaQueryThread(&lua_query));
+    TEST_ASSERT(!memcmp(&lua_query, &test_lua_query, sizeof(struct LuaQuery)));
+END_TEST
 
-    TEST_SUCCESS("test_StopLuaQueryThread");
-    return 0;
-}
-
-static int
-test_stopAndStartLuaQueryThread(struct lua_State *const L)
-{
+TEST(test_stopAndStartLuaQueryThread)
     struct LuaQuery lua_query, test_lua_query;
     memset(&lua_query, 0xDEADBEEF, sizeof(struct LuaQuery));
     memcpy(&test_lua_query, &lua_query, sizeof(struct LuaQuery));
@@ -230,36 +230,26 @@ test_stopAndStartLuaQueryThread(struct lua_State *const L)
     lua_query.thread = NO_THREAD;
     test_lua_query.persist.host_data =
         lua_query.persist.host_data =   &host_data;
-    assert(startLuaQueryThread(&lua_query));
-    assert(stopLuaQueryThread(&lua_query));
+    TEST_ASSERT(startLuaQueryThread(&lua_query));
+    TEST_ASSERT(stopLuaQueryThread(&lua_query));
     test_lua_query.thread = lua_query.thread;
-    assert(!memcmp(&lua_query, &test_lua_query, sizeof(struct LuaQuery)));
+    TEST_ASSERT(!memcmp(&lua_query, &test_lua_query, sizeof(struct LuaQuery)));
+END_TEST
 
-    TEST_SUCCESS("test_StopAndStartLuaQuery");
-    return 0;
-}
-
-static int
-test_destroyLuaQuery(struct lua_State *const L)
-{
+TEST(test_destroyLuaQuery)
     struct HostData *host_data =
         createHostData("something", "else", "isunderthebed!", 199);
-    assert(host_data);
+    TEST_ASSERT(host_data);
 
     struct LuaQuery *lua_query = createLuaQuery(&host_data);
-    assert(lua_query);
+    TEST_ASSERT(lua_query);
 
     destroyLuaQuery(&lua_query);
-    assert(NULL == lua_query);
+    TEST_ASSERT(NULL == lua_query);
+END_TEST
 
-    TEST_SUCCESS("test_destroyLuaQuery");
-    return 0;
-}
-
-static int
-test_restartLuaQuery(struct lua_State *const L)
-{
-    assert(MAX_RESTARTS > 0);
+TEST(test_restartLuaQuery)
+    TEST_ASSERT(MAX_RESTARTS > 0);
 
     const char *const init_host     = "127.0.0.1";
     const char *const init_user     = "where";
@@ -268,66 +258,56 @@ test_restartLuaQuery(struct lua_State *const L)
 
     struct HostData *host_data =
         createHostData(init_host, init_user, init_passwd, init_port);
-    assert(host_data);
+    TEST_ASSERT(host_data);
 
     struct LuaQuery *lua_query = createLuaQuery(&host_data);
-    assert(lua_query);
+    TEST_ASSERT(lua_query);
 
-    assert(restartLuaQueryThread(lua_query));
-    assert(false            == lua_query->command_ready);
-    assert(false            == lua_query->completion_signal);
-    assert(-1               == lua_query->command);
-    assert(NULL             == lua_query->query);
-    assert(-1               == lua_query->output_count);
-    assert(!strcmp(init_host, lua_query->persist.host_data->host));
-    assert(!strcmp(init_user, lua_query->persist.host_data->user));
-    assert(!strcmp(init_passwd, lua_query->persist.host_data->passwd));
-    assert(init_port        == lua_query->persist.host_data->port);
-    assert(1                == lua_query->persist.restarts);
-    assert(NULL             == lua_query->persist.ell);
+    TEST_ASSERT(restartLuaQueryThread(lua_query));
+    TEST_ASSERT(false            == lua_query->command_ready);
+    TEST_ASSERT(false            == lua_query->completion_signal);
+    TEST_ASSERT(-1               == lua_query->command);
+    TEST_ASSERT(NULL             == lua_query->query);
+    TEST_ASSERT(-1               == lua_query->output_count);
+    TEST_ASSERT(!strcmp(init_host, lua_query->persist.host_data->host));
+    TEST_ASSERT(!strcmp(init_user, lua_query->persist.host_data->user));
+    TEST_ASSERT(!strcmp(init_passwd, lua_query->persist.host_data->passwd));
+    TEST_ASSERT(init_port        == lua_query->persist.host_data->port);
+    TEST_ASSERT(1                == lua_query->persist.restarts);
+    TEST_ASSERT(NULL             == lua_query->persist.ell);
 
-    assert(stopLuaQueryThread(lua_query));
+    TEST_ASSERT(stopLuaQueryThread(lua_query));
+END_TEST
 
-    TEST_SUCCESS("test_restartLuaQuery");
-    return 0;
-}
-
-static int
-test_zombie(struct lua_State *const L)
-{
+TEST(test_zombie)
     struct LuaQuery lua_query, test_lua_query;
     memset(&lua_query, 0x11111111, sizeof(struct LuaQuery));
     memcpy(&test_lua_query, &lua_query, sizeof(struct LuaQuery));
 
-    assert(MAX_RESTARTS >= 0);
+    TEST_ASSERT(MAX_RESTARTS >= 0);
     if (MAX_RESTARTS > 0) {
         test_lua_query.persist.restarts =
             lua_query.persist.restarts =        MAX_RESTARTS - 1;
-        assert(false == zombie(&lua_query));
-        assert(!memcmp(&lua_query, &test_lua_query,
+        TEST_ASSERT(false == zombie(&lua_query));
+        TEST_ASSERT(!memcmp(&lua_query, &test_lua_query,
                        sizeof(struct LuaQuery)));
     }
 
     test_lua_query.persist.restarts =
         lua_query.persist.restarts =            MAX_RESTARTS;
-    assert(true == zombie(&lua_query));
-    assert(!memcmp(&lua_query, &test_lua_query, sizeof(struct LuaQuery)));
+    TEST_ASSERT(true == zombie(&lua_query));
+    TEST_ASSERT(!memcmp(&lua_query, &test_lua_query, sizeof(struct LuaQuery)));
 
     // will cause abort
     /*
     test_lua_query.persist.restarts =
         lua_query.persist.restarts =            MAX_RESTARTS + 1;
-    assert(true == zombie(&lua_query));
-    assert(!memcmp(&lua_query, &test_lua_query, sizeof(struct LuaQuery)));
+    TEST_ASSERT(true == zombie(&lua_query));
+    TEST_ASSERT(!memcmp(&lua_query, &test_lua_query, sizeof(struct LuaQuery)));
     */
+END_TEST
 
-    TEST_SUCCESS("test_zombie");
-    return 0;
-}
-
-static int
-test_createHostData(struct lua_State *const L)
-{
+TEST(test_createHostData)
     const char *const init_host     = "birdsandplains";
     const char *const init_user     = "automobiles";
     const char *const init_passwd   = "sendandreceive";
@@ -342,40 +322,32 @@ test_createHostData(struct lua_State *const L)
 
     struct HostData *const host_data =
         createHostData(init_host, init_user, init_passwd, init_port);
-    assert(host_data);
+    TEST_ASSERT(host_data);
 
-    assert(!strcmp(test_host_data.host, host_data->host));
-    assert(!strcmp(test_host_data.user, host_data->user));
-    assert(!strcmp(test_host_data.passwd, host_data->passwd));
-    assert(test_host_data.port == host_data->port);
-    assert(init_port           == host_data->port);
+    TEST_ASSERT(!strcmp(test_host_data.host, host_data->host));
+    TEST_ASSERT(!strcmp(test_host_data.user, host_data->user));
+    TEST_ASSERT(!strcmp(test_host_data.passwd, host_data->passwd));
+    TEST_ASSERT(test_host_data.port == host_data->port);
+    TEST_ASSERT(init_port           == host_data->port);
 
     // make sure createHostData didn't mutate values it doesn't own
-    assert(init_host    != host_data->host);
-    assert(init_user    != host_data->user);
-    assert(init_passwd  != host_data->passwd);
+    TEST_ASSERT(init_host    != host_data->host);
+    TEST_ASSERT(init_user    != host_data->user);
+    TEST_ASSERT(init_passwd  != host_data->passwd);
 
-    assert(!strcmp(test_host_data.host, init_host));
-    assert(!strcmp(test_host_data.user, init_user));
-    assert(!strcmp(test_host_data.passwd, init_passwd));
+    TEST_ASSERT(!strcmp(test_host_data.host, init_host));
+    TEST_ASSERT(!strcmp(test_host_data.user, init_user));
+    TEST_ASSERT(!strcmp(test_host_data.passwd, init_passwd));
+END_TEST
 
-    TEST_SUCCESS("test_createHostData");
-    return 0;
-}
-
-static int
-test_destroyHostData(struct lua_State *const L)
-{
+TEST(test_destroyHostData)
     struct HostData *host_data =
         createHostData("some", "data", "willdo", 1005);
-    assert(host_data);
+    TEST_ASSERT(host_data);
 
     destroyHostData(&host_data);
-    assert(NULL == host_data);
-
-    TEST_SUCCESS("test_destroyHostData");
-    return 0;
-}
+    TEST_ASSERT(NULL == host_data);
+END_TEST
 
 static int
 all(struct lua_State *const L)
