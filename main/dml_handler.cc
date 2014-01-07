4017cad1eacc4dae88818555097f2e38bc6a561a
burrows <burrows.labs@gmail.com>
2014-01-07 14:06:51 -0500

    fixed the copy for bad onion adjustment directives

--- main/dml_handler.cc
+++ main/dml_handler.cc
@@ -1048,7 +1048,7 @@ private:
                 const FieldMeta &fm = a.getFieldMeta(tm, field);
                 throw OnionAdjustExcept(tm, fm, o.get(), l.get());
             } else if (l.get() > current_level) {
-                FAIL_TextMessageError("it is not possible to add onions; only remove them");
+                FAIL_TextMessageError("it is not possible to add layers; only remove them");
             }
         }
 
